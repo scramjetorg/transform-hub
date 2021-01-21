037be70d9b90101525f424a4869b761192e307eb
MichalCz <cz@signicode.com>
2021-01-21 18:53:26 +0000

    Add Supervisor stub.

---
+++ src/supervisor/index.ts
@@ -0,0 +1,12 @@
+import {promisify} from "util";
+import {access} from "fs";
+import { resolve } from "path";
+
+const supervisor = async () => {
+    console.log("aaaa");
+    console.log(
+        await promisify(access)(resolve(__dirname, 'package.json'))
+    );
+};
+
+export {supervisor};
