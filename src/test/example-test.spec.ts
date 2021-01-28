6247be46de9e8a968bf6b9be572dc53965057d8e
MichalCz <cz@signicode.com>
2021-01-28 17:57:15 +0000

    Fix some minor eslint issues.

--- src/test/example-test.spec.ts
+++ src/test/example-test.spec.ts
@@ -1,18 +1,18 @@
-const test = require('ava');
+const test = require("ava");
 
 test.before(async t => {
-	t.context.letter = "g";
+    t.context.letter = "g";
 });
 
-test('passing test', t => {
-	t.pass();
+test("passing test", t => {
+    t.pass();
 });
 
-test('context test', t => {
-	t.is(t.context.letter, 'g');
+test("context test", t => {
+    t.is(t.context.letter, "g");
 });
 
-test('async test', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
+test("async test", async t => {
+    const bar = Promise.resolve("bar");
+    t.is(await bar, "bar");
 });
