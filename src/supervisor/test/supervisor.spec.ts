13ec5aa2b6d11709cf3d184f4689dcb94239f831
Budleigh Salterton <cz@scramjet.org>
2021-02-05 16:02:40 +0000

    Fixed test

--- src/supervisor/test/supervisor.spec.ts
+++ src/supervisor/test/supervisor.spec.ts
@@ -1,28 +1,30 @@
-import test from 'ava';
-import { StringStream } from 'scramjet';
-import { spawn } from 'child_process';
+import test from "ava";
+import { StringStream } from "scramjet";
+import { spawn } from "child_process";
 
-const testSocket = "/tmp/test-socketx.sock";
+const testSocket = `/tmp/test-socket-${process.pid}.sock`;
 
 test("start supervisor on " + testSocket, async t => {
     const child = spawn("/usr/bin/env", ["npx", "ts-node", "bin/supervisor.ts", testSocket]);
 
-    await new Promise(async (resolve) => {
-        //TODO const data = await StringStream.from(child.stdout).whenRead(10);
-        setTimeout(async () => {
+    await new Promise(async(resolve) => {
+        setTimeout(async() => {
             spawn("/usr/bin/env", ["rm", testSocket]);
             child.kill(9);
-        }, 3000);
+        }, 8000);
 
-        child.on('exit', function (code: any, signal: any) {
-            if (code == 1) {
-                t.fail('child process exited with ' +
-                `code ${code} and signal ${signal}`);
-            } else if (code == 0 || 'SIGKILL' == signal) {
+        const data = await StringStream.from(child.stdout).lines().slice(0, 1).whenRead();
+        console.log(data);
+
+        child.on("exit", function(code: any, signal: any) {
+            if (code === 1) {
+                t.fail("child process exited with " +
+                    `code ${code} and signal ${signal}`);
+            } else if (code === 0 || signal === "SIGKILL") {
                 t.pass();
             }
 
             resolve(1);
-        })
+        });
     });
 });
