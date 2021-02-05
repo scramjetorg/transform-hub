27cbd74a782796eadf74a9f17bab7a7bc39f8cb4
Budleigh Salterton <cz@scramjet.org>
2021-02-05 15:21:55 +0000

    Work in progress

--- src/types/runner.ts
+++ src/types/runner.ts
@@ -5,13 +5,13 @@ import { exposeSequenceSymbol } from "@scramjet/symbols";
  * @ignore
  */
 type Gen<W, R, C extends any[] = []> =
-    (...config: C) => Generator<R, W|void, W>;
+    (...config: C) => Generator<R, W | void, W>;
 /**
  * This is a polyfill to TypeScripts rather poor expression of `async function*`
  * @ignore
  */
 type AsyncGen<W, R, C extends any[] = []> =
-    (...config: C) => AsyncGenerator<R, W|void, W>;
+    (...config: C) => AsyncGenerator<R, W | void, W>;
 
 /**
  * This is a simple utility type for an argument for `Promise.resolve`
@@ -101,69 +101,69 @@ export type TFunction<Consumes, Produces> =
     AsyncGen<Produces, Consumes> |
     Gen<Produces, Consumes> |
     TranformFunction<Consumes, Produces>
-;
+    ;
 export type WFunction<Consumes> = TFunction<Consumes, never>;
 
 type ArrayMin<Y, X, W, V extends any[]> = [] | [Y] | [Y, X] | [Y, X, W, V];
 
 // @ts-ignore
-type MulTFunction<T, S, W=any, X=any, Y=any> =
+type MulTFunction<T, S, W = any, X = any, Y = any> =
     [TFunction<T, S>] |
     [TFunction<T, Y>, TFunction<Y, S>] |
     [TFunction<T, Y>, TFunction<Y, X>, TFunction<X, S>] |
     [TFunction<T, Y>, TFunction<Y, X>, TFunction<X, W>, TFunction<W, S>] |
     never
-;
+    ;
 
 // @ts-ignore
 // eslint-disable-next-line no-use-before-define, @typescript-eslint/no-unused-vars
-type MulMulTFunction<T, S, Z extends ArrayMin<[Y, X, W, ...V, ...U]>, Y=any, X=any, W=any, V=any[], U=any[]> =
+type MulMulTFunction<T, S, Z extends ArrayMin<[Y, X, W, ...V, ...U]>, Y = any, X = any, W = any, V = any[], U = any[]> =
     [TFunction<T, Y>, TFunction<Y, S>] |
     [TFunction<T, Y>, TFunction<Y, X>, TFunction<X, S>] |
     [TFunction<T, Y>, ...MulTFunction<Y, X, V>, TFunction<X, S>] |
     [TFunction<T, Y>, ...MulTFunction<Y, X, U>, ...MulTFunction<X, W, V>, TFunction<W, S>]
-;
+    ;
 
 type TFunctionChain<Consumes, Produces, Z extends any[]> =
     [TFunction<Consumes, Produces>] |
     [...MulMulTFunction<Consumes, Produces, Z>]
-;
+    ;
 
 /**
  * Minimal type of Sequence that doesn't read anything from the outside, doesn't
  * write anything to outside. It may be doing anything, but it's not able to report
  * the progress via streaming.
  */
-export type InertSequence<Z=any, Y=any, X extends any[] = any[]> = TFunctionChain<Y, Z, X>;
+export type InertSequence<Z = any, Y = any, X extends any[] = any[]> = TFunctionChain<Y, Z, X>;
 
 /**
  * A Sequence of functions that accept some input, transforms it through
  * a number of functions and writes to some destination.
  */
-export type WriteSequence<Consumes, Y extends any[] = any[], Z=any> =
+export type WriteSequence<Consumes, Y extends any[] = any[], Z = any> =
     [WFunction<Consumes>] |
     [...TFunctionChain<Consumes, Z, Y>, WFunction<Z>] |
     [RFunction<Consumes>, WFunction<Consumes>] |
     [RFunction<Consumes>, ...TFunctionChain<Consumes, Z, Y>, WFunction<Z>]
-;
+    ;
 
 /**
  * A sequence of functions reads input from a source and outputs it after
  * a chain of transforms.
  */
-export type ReadSequence<Produces, Y extends any[] = any[], Z=any> =
+export type ReadSequence<Produces, Y extends any[] = any[], Z = any> =
     [RFunction<Produces>] |
     [RFunction<Z>, ...TFunctionChain<Z, Produces, Y>]
-;
+    ;
 
 /**
  * A Transform Sequence is a sequence that accept input, perform operations on it, and
  * outputs the result. 
  */
-export type TransformSeqence<Consumes, Produces, Z=any, X extends any[] = any[]> =
+export type TransformSeqence<Consumes, Produces, Z = any, X extends any[] = any[]> =
     [TFunction<Consumes, Produces>] |
     [...TFunctionChain<Consumes, Z, X>, TFunction<Z, Produces>]
-;
+    ;
 
 /**
  * Defines scalability options for writable or readable side of the Function:
@@ -211,24 +211,58 @@ export type FunctionDefinition = {
     }
 }
 
+/**
+ * Provides basic function status information
+ */
 export type FunctionStatus = {
+    /**
+     * Average number of stream entries passing that specific function over the duration 
+     * of 1 second during the last 10 seconds.
+     */
     throughput: number;
-    pressure: number;
+    /**
+     * The amount of stream entries that this function will accept in queue for processing
+     * before `pause` is called (i.e. highWaterMark - processing)
+     */
+    buffer: number;
+    /**
+     * The number of stream entries currently being processed.
+     */
+    processing: number;
+    /**
+     * Calculated backpressure: processing * throughput / buffer
+     */
+    readonly pressure: number;
 }
 
-export type MonitoringResponse = {
+/**
+ * The response an Sequence sends as monitoring responses
+ */
+export type MonitoringMessage = {
+    /**
+     * 
+     */
     sequences?: FunctionStatus[];
     healthy?: boolean;
 };
 
-export type AppConfig = {[key: string]: null|string|number|boolean|AppConfig};
+/**
+ * App configuration primitive.
+ */
+export type AppConfig = { [key: string]: null | string | number | boolean | AppConfig };
 
+/**
+ * Acceptable error codes
+ */
 export type AppErrorCode =
     "GENERAL_ERROR" |
     "COMPILE_ERROR" |
     "SEQUENCE_MISCONFIGURED"
-;
+    ;
 
+/**
+ * Application error class
+ */
 export type AppError = Error & {
     code: AppErrorCode;
     exitcode?: number;
@@ -242,46 +276,198 @@ export type AppError = Error & {
  */
 type AppErrorConstructor = new (code: AppErrorCode, message?: string) => AppError;
 
-export interface App2Context<AppConfigType extends AppConfig> {
-    monitor?: (resp: MonitoringResponse) => MaybePromise<MonitoringResponse>;
-    describe?: (tx: FunctionDefinition[]) => void;
+/**
+ * Object of this interface is passed to Application context and allows it to communicate
+ * with the Platform to ensure that it's in operation and should be kept alive without 
+ * interruption.
+ * 
+ * 
+ */
+interface AutoApp2Context<AppConfigType extends AppConfig, State extends any> {
+    /**
+     * This method should be overridden by the Sequence if auto detection of the Sequence
+     * state is not precise enough.
+     * 
+     * The Runner will call this message periodically to obtain information on the 
+     * condition of the Sequence.
+     * 
+     * If not provided, a monitoring function will be determined based on the 
+     * return value from the Sequence.
+     * 
+     * @param resp passed if the system was able to determine monitoring message by itself.
+     * @returns the monitoring information
+     */
+    monitor?: (resp?: MonitoringMessage) => MaybePromise<MonitoringMessage>;
 
-    stopHandler?: () => Promise<void>;
-    killHandler?: () => Promise<void>;
+    /**
+     * This method can be overridden to handle the stop signal from the Runner and perform
+     * a graceful shutdown. The platform will provide a grace period in order to save current
+     * work and provide. 
+     * 
+     * This method can be called under two conditions:
+     * * the instance will be terminated for extraneous reasons
+     * * the instance was not able to confirm if the Sequence is alive
+     * 
+     * The method can call @{see this.state} as many times as it likes, the valkue from the
+     * last call will be made sure to be saved before the process will be terminated.
+     * 
+     * @param timeout the number of seconds before the operation will be killed
+     * @param canCallKeepalive informs if keepAlive can be called to prolong the operation
+     * @returns the returned value can be a promise, once it's resolved the system will
+     *          assume that it's safe to terminate the process.
+     */
+    stopHandler?: (timeout: number, canCallKeepalive: boolean) => MaybePromise<void>;
 
+    /**
+     * This method can be overridden to handle the kill signal from the Runner and perform
+     * the final cleanup. This method is synchroneous and once it's exits the process will be
+     * synchronously terminated.
+     * 
+     * The method can call @{see this.state} as many times as it likes, the valkue from the
+     * last call will be made sure to be saved before the process will be terminated.
+     * 
+     * If this methods fails to exit within `100 ms` the process will be forcefully terminated
+     * and the data will be lost.
+     */
+    killHandler?: () => void;
+
+    /**
+     * The Sequence may call this process in order to confirm continued operation and provide
+     * an information on how long the Sequence should not be considered stale.
+     * 
+     * Sequences that expose read or write functions do not need to call this when the data
+     * is flowing.
+     * 
+     * If the platform finds no indication that the Sequence is still alive it will attempt to
+     * stop it.
+     * 
+     * @param milliseconds provides information on how long the process should wait before
+     */
     keepAlive(milliseconds?: number): this;
-    end(): this;
-    destroy(): this;
 
+    /**
+     * Calling this method will inform the Instance that the Sequence has completed the 
+     * operation and can be gracefully terminated.
+     * 
+     * If the Sequence is Writable or Inert then it will call the stopHandler immediatelly.
+     * If the Sequence is Readable of Transform then it will call the stopHandler when all the
+     * data is passed to the neighours.
+     * 
+     * This method will be called automatically when the readable side of the sequence ends.
+     */
+    end(): this;
+    /**
+     * Calling this method will inform the Instance that the Sequence has enountered a fatal
+     * exception and should not be kept alive.
+     * 
+     * If the Sequence is Writable or Inert then it will call the stopHandler immediatelly.
+     * If the Sequence is Readable of Transform then it will call the stopHandler when any 
+     * error occurs on the readable side or it reaches end and all the data is passed to the 
+     * neighours.
+     * 
+     * This method will be called automatically when the readable side of the sequence errors 
+     * out.
+     * 
+     * @param error optional error object for inspection
+     */
+    destroy(error?: AppError): this;
+
+    /**
+     * This method can be used to ensure that the data is passed here will be stored for 
+     * another process. The operation is synchroneous and blocking.
+     * 
+     * You can call the method as many times as you want, the last state will be safely
+     * passed to the process that will take over after this one is terminated.
+     * 
+     * @param state any serializable value
+     */
+    save(state: State): void;
+
+    /**
+     * Holds the previous state if there was a previous process in existance and it called the
+     * {@link this#save}.
+     */
+    initialState?: State;
+
+    /**
+     * Receives events sent by the Instance that can be triggered via CLI and configured 
+     * actions.
+     * 
+     * @param ev 
+     * @param handler 
+     */
     on(ev: string, handler: (message?: any) => void): this;
     on(ev: "error", handler: (message: Error) => void): this;
 
+    /**
+     * Sends events to the Instance that can be received by CLI and configured actions
+     * 
+     * @param ev event name
+     * @param message any serializable object
+     */
     emit(ev: string, message?: any): this;
     emit(ev: "error", message: AppError): this;
 
+    /**
+     * Provides automated definition as understood by 
+     */
     definition?: FunctionDefinition[];
+
     readonly config: AppConfigType;
     readonly AppError: AppErrorConstructor;
 }
+
+/**
+ * Object of this interface is passed to Application context and allows it to communicate
+ * with the Platform to ensure that it's in operation and should be kept alive without 
+ * interruption.
+ */
+interface FullApp2Context<
+    AppConfigType extends AppConfig,
+    State extends any
+> extends AutoApp2Context<AppConfigType, State> {
+    /**
+     * This method must be overridden by the Sequence if definition is not 
+     * providing the correct outcome.
+     * 
+     * The Runner will call this message periodically to obtain information on the 
+     * condition of the Sequence.
+     * 
+     * If not provided, a monitoring function will be determined based on the 
+     * return value from the Sequence.
+     * 
+     * @param resp passed if the system was able to determine monitoring message by itself.
+     * @returns the monitoring information
+     */
+    monitor: (resp?: MonitoringMessage) => MaybePromise<MonitoringMessage>;
+    /**
+     * This method should be overridden by the sequence if auto detection of the Sequence
+     * definition is not providing the correct outcome.
+     */
+    describe: () => FunctionDefinition[];
+}
+
+export type App2Context<AppConfigType extends AppConfig, State extends any> =
+    AutoApp2Context<AppConfigType, State> | FullApp2Context<AppConfigType, State>;
 
 type TransformAppAcceptableSequence<Consumes, Produces> =
     TFunction<Consumes, Produces> |
     InertSequence |
     TransformSeqence<Consumes, Produces>
-;
+    ;
 
 export type TransformApp<
     Consumes = any,
     Produces = any,
     Z extends any[] = any[],
     AppConfigType extends AppConfig = AppConfig
-> =
-   (
+    > =
+    (
         this: App2Context<AppConfigType>,
         source: ReadableStream<Consumes>,
         ...args: Z
     ) => MaybePromise<TransformAppAcceptableSequence<Consumes, Produces>>
-;
+    ;
 
 export type ReadableApp<Produces = any, Z extends any[] = any[], AppConfigType extends AppConfig = AppConfig> =
     (
@@ -289,39 +475,39 @@ export type ReadableApp<Produces = any, Z extends any[] = any[], AppConfigType e
         source: ReadableStream<never>,
         ...args: Z
     ) => MaybePromise<RFunction<Produces> | ReadSequence<Produces>>
-;
+    ;
 
 export type WritableApp<Consumes = any, Z extends any[] = any[], AppConfigType extends AppConfig = AppConfig> =
     (
         this: App2Context<AppConfigType>,
         source: ReadableStream<Consumes>,
         ...args: Z
-    ) => MaybePromise<WFunction<Consumes>|WriteSequence<Consumes>|void>
-;
+    ) => MaybePromise<WFunction<Consumes> | WriteSequence<Consumes> | void>
+    ;
 
 export type InertApp<Z extends any[] = any[], AppConfigType extends AppConfig = AppConfig> =
-(
-    this: App2Context<AppConfigType>,
-    source: ReadableStream<void>,
-    ...args: Z
-) => MaybePromise<WFunction<void>|WriteSequence<void>|void>
-;
+    (
+        this: App2Context<AppConfigType>,
+        source: ReadableStream<void>,
+        ...args: Z
+    ) => MaybePromise<WFunction<void> | WriteSequence<void> | void>
+    ;
 
 export type ApplicationExpose<
     Consumes = any, Produces = any, Z extends any[] = any[],
     AppConfigType extends AppConfig = AppConfig
-> = {
-    // Because we need this a bit lower again, so it's an egg-hen problem.
-    // eslint-disable-next-line no-use-before-define
-    [exposeSequenceSymbol]: Application<Consumes, Produces, Z, AppConfigType>;
-};
+    > = {
+        // Because we need this a bit lower again, so it's an egg-hen problem.
+        // eslint-disable-next-line no-use-before-define
+        [exposeSequenceSymbol]: Application<Consumes, Produces, Z, AppConfigType>;
+    };
 
 export type Application<
     Consumes = any,
     Produces = any,
     Z extends any[] = any[],
     AppConfigType extends AppConfig = AppConfig
-> =
+    > =
     TransformApp<Consumes, Produces, Z, AppConfigType> |
     ReadableApp<Produces, Z, AppConfigType> |
     WritableApp<Consumes, Z, AppConfigType> |
@@ -346,4 +532,4 @@ export type RunnerMessage = [
 
 export type RunnerOptions = {
     monitoringInterval?: number
-}
+};
