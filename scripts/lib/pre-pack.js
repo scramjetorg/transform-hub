f95e5dbf2e6dec4c706ac59a2056ebc5e271755b
Agnieszka Wypich <agne6ka@gmail.com>
2021-02-10 15:02:43 +0100

    Add move packages to root dist.

--- scripts/lib/pre-pack.js
+++ scripts/lib/pre-pack.js
@@ -1,32 +1,51 @@
 const fs = require("fs"),
+    fse = require("fs-extra"),
     path = require("path");
 
 class PrePack {
     build() {
-        this.dir = process.cwd();
+        this.currDir = process.cwd();
+        this.folderName = this.currDir.split(path.sep).pop();
         this.rootDir = path.join(__dirname, "../../");
-        this.distDir = path.join(this.dir, "/dist");
-        this.copyLicense();
-        this.handleJson();
+        this.rootDist = path.join(this.rootDir, "/dist");
+        this.currDirDist = path.join(this.currDir, "/dist");
+
+        this.readJson()
+            .then(this.writeJson.bind(this))
+            .then(this.handleDistFiles.bind(this))
+            .catch(message => console.log(message));
     }
-    copyLicense() {
-        fs.copyFile(`${this.rootDir}/LICENSE`, `${this.distDir}/LICENSE`, (err) => {
-            if (err) throw err;
-            console.log(`Licence was copy form ${this.rootDir} to ${this.distDir}`);
-        });
-    }
-    handleJson() {
+    handleDistFiles() {
         return new Promise((resolve, reject) => {
-            this.readJson()
-                .then(this.writeJson.bind(this))
+            this.copyFile(this.rootDir, this.currDirDist, "LICENSE")
+                .then(this.copyFolder.bind(this, this.currDirDist, this.rootDist))
                 .then(resolve)
                 .catch(reject);
         });
     }
+    copyFolder(input, output) {
+        console.log(`Copy files form ${input} to ${output}`);
+        return new Promise((resolve, reject) => {
+            fse.copy(`${input}/`, `${output}/${this.folderName}`, (err) => {
+                if (err) reject(`Unable to copy folder form ${input} to ${output}, error code: ${err.code}`);
+                else resolve();
+            });
+        });
+    }
+    copyFile(input, output, fileName) {
+        console.log(`Copy ${fileName} file form ${input} to ${output}`);
+        return new Promise((resolve, reject) => {
+            if (typeof fileName !== "string") reject(`${fileName} is invalid type name`);
+            fs.copyFile(`${input}/${fileName}`, `${output}/${fileName}`, (err) => {
+                if (err) reject(`Unable to copy ${fileName} form ${input} to ${output}, error code: ${err.code}`);
+                else resolve();
+            });
+        });
+    }
     readJson() {
         return new Promise((resolve, reject) => {
-            fs.readFile(`${this.dir}/package.json`, (err, content) => {
-                if (err) reject(`Unable to read package.json error code: ${err.code}`);
+            fs.readFile(`${this.currDir}/package.json`, (err, content) => {
+                if (err) reject(`Unable to read package.json, error code: ${err.code}`);
                 else {
                     try {
                         this.jsonFile = JSON.parse(content);
@@ -39,15 +58,15 @@ class PrePack {
         });
     }
     editJson() {
-        console.log(`Path: ${this.dir}. Editing package.json.`);
+        console.log(`Path: ${this.currDir}. Editing package.json.`);
         this.jsonFile.scripts = {};
         this.jsonFile.devDependencies = {};
     }
     writeJson() {
         this.editJson();
-        console.log(`Add package.json to ${this.dir}`);
+        console.log(`Add package.json to ${this.currDir}`);
         return new Promise((resolve, reject) => {
-            fs.writeFile(`${this.distDir}/package.json`, JSON.stringify(this.jsonFile, null, 2), "utf8", (err) => {
+            fs.writeFile(`${this.currDirDist}/package.json`, JSON.stringify(this.jsonFile, null, 2), "utf8", (err) => {
                 if (err) reject(`Unable to overwrite ${this.jsonFile} package.json`);
                 else resolve();
             });
