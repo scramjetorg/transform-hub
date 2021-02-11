3f6ef7feb235f66266d4d56366d0f5573642d292
urbanowiczm <urbanowicz.maciej@gmail.com>
2021-02-11 12:15:36 +0100

    Add comments to supervisor running scripts. (#31)

--- src/supervisor/bin/supervisor.ts
+++ src/supervisor/bin/supervisor.ts
@@ -1,19 +1,28 @@
 import server from "../lib/server";
-
 const fs = require("fs");
-const socket: string = process.argv[2];
+const socketName: string = process.argv[2];
 
-if (fs.existsSync(socket)) {
-    console.error("Socket file already exists. " + socket);
+/**
+ * Supervisor running script. 
+ * Creates an instance of a new http server. 
+ * The server provides read/write communication through named unix socket passed as a command-line argument.  
+ * 
+ * @returns
+ * Script prints to stdout socket name.
+ * If socket file exists or an error occured, exit code is returned.
+ */
+
+if (fs.existsSync(socketName)) {
+    console.error("Socket file already exists. " + socketName);
     process.exit(1);
 }
 
-server(socket)
+server(socketName)
     .on("error", (err: Error) => {
         console.error(err);
         //TODO check process exitCode -  
         process.exit(1);
     })
-    .listen(socket, () => {
-        console.log("listening on socket", socket);
+    .listen(socketName, () => {
+        console.log("listening on socket", socketName);
     });
--- src/supervisor/lib/server.ts
+++ src/supervisor/lib/server.ts
@@ -1,6 +1,16 @@
 import { createServer, IncomingMessage, Server } from "http";
 
-const makeServer: (socket: string) => Server = () => {
+/**
+ * Creates an instance of a new http server. 
+ * The server listens on a socket specified by unix path passed as an argument. 
+ *
+ * @example makeServer("/tmp/unix-socket.sock");
+ * @param {string} socket The unix socket path which will be used for communication  
+ * 
+ * @returns {Server} New http server instance.
+ */
+
+const makeServer: (socketName: string) => Server = () => {
     return createServer((message: IncomingMessage) => {
         message.on("data", (data) => {
             console.log("data" + data.toString());
