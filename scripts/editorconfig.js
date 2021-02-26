350fc80164a30120941dfa37052579f3f9abafef
Budleigh Salterton <cz@scramjet.org>
2021-02-26 10:29:41 +0000

    Automate editorconfig creation

---
+++ scripts/editorconfig.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+// This script regenerates editorconfig based on eslint rules.
+
+const transform = require("eslint-to-editorconfig");
+const { ESLint } = require("eslint");
+const { resolve } = require("path");
+const { writeFile } = require("fs/promises");
+
+(async() => {
+    const { rules } = await new ESLint({ cwd: resolve(__dirname, "../") })
+        .calculateConfigForFile(".eslintrc");
+
+    // Transform them into EditorConfig rules
+    const editorConfigRules = transform(rules);
+    await writeFile(
+        resolve(__dirname, "../.editorconfig.json"),
+        JSON.stringify(editorConfigRules, null, 2)
+    );
+})()
+    .catch(e => {
+        console.error(e.stack);
+        process.exitCode = e.exitCode || 10;
+    });
