9f0b5839a1f4464d63995720e96d3be150171cd2
Budleigh Salterton <cz@scramjet.org>
2021-03-01 13:47:58 +0000

    Test fix

--- packages/supervisor/src/test/csh-client.spec.ts
+++ packages/supervisor/src/test/csh-client.spec.ts
@@ -4,21 +4,26 @@ import test from "ava";
 const testPath = "...";
 
 test("load package form path " + testPath, async _t => {
+    _t.pass();
     // Test if getPackage method load the package.
 });
 
 test("return readable stream with package ", async _t => {
+    _t.pass();
     // Test if getPackage method returns a readable stream.
 });
 
 test("get stream array", async _t => {
+    _t.pass();
     // Test if getClient method takes an array.
 });
 
 test("array contains proper elements", async _t => {
+    _t.pass();
     // Test if array passed to getClient method contains stdio, stderr and controllers.
 });
 
 test("return to the console", async _t => {
+    _t.pass();
     // Test if getClient method return stream to the console.
 });
