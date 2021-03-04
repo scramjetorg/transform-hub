7abf7af10abf07972e0f4b8ccb89d68a87f93588
Ubuntu <alicja.gruzdz@scramjet.org>
2021-03-04 10:22:30 +0000

    Remove spy test due to unhandled error, add comments

--- packages/supervisor/src/lib/lifecycle-controller.ts
+++ packages/supervisor/src/lib/lifecycle-controller.ts
@@ -19,35 +19,89 @@ import { CommunicationHandler } from "@scramjet/model/src/stream-handler";
  */
 class LifeCycleController {
 
+    /**
+    * CommunicationHandler is responsible for all operations on communication streams.
+    * @type {CommunicationHandler}
+    */
     private communicationHandler: CommunicationHandler = new CommunicationHandler();
+
+    /**
+    * CSH Client handles communication with CSH
+    * @type {CSHClientMock}
+    */
     private client: CSHClientMock = new CSHClientMock();
+
+    /**
+    * Interface specifing methods that can be performed during Supervisor's lifecycle. 
+    * @type {LifeCycleMock}
+    */
     private lifecycleAdapterMock: LifeCycleMock;
+
+    /**
+    * Configurations specific to a lifecycle, e.g. whether to take snapshot in case of erroneous Sequence termination.
+    * @type {LifeCycleConfig}
+    */
     private lifecycleConfig: LifeCycleConfig;
 
+    /**
+     * @param {LifeCycleMock} lifecycleAdapter an implemenation of LifeCycle interface
+     * @param {LifeCycleConfig} lifecycleConfig configuration specific to this lifecycle
+     */
     constructor(lifecycleAdapter: LifeCycleMock, lifecycleConfig: LifeCycleConfig) {
         this.lifecycleAdapterMock = lifecycleAdapter;
         this.lifecycleConfig = lifecycleConfig;
     }
 
+    /**
+     * Starts Supervisors's lifecycle.
+     *
+     * @returns {Promise} resolves when Supervisor completed lifecycle without errors.
+     */
     async start(): Promise<void> {
 
         try {
 
+            /**
+            * Receive a readable stream to compressed Sequence code and configuration file
+            */
             const packageStream = this.client.getPackage();
 
+            /**
+            * LifeCycle Adapter calls identify method to unpack the compressed file 
+            * and obtain the Runner's container configuration
+            */
             const config = await this.lifecycleAdapterMock.identify(packageStream);
 
+            /**
+            *  Pass CommunicationHandler to CSH Client so it can hook its upstreams to it
+            */
             this.client.hookCommunicationHandler(this.communicationHandler);
 
+            /**
+            *  Pass CommunicationHandler to LifeCycle Adapter so it can hook its downstreams to it
+            */
             this.lifecycleAdapterMock.hookCommunicationHandler(this.communicationHandler);
 
+            /**
+            * Pipe control and monitor streams between CSH Client and LifeCycle Adapter
+            */
             this.communicationHandler.pipeMessageStreams();
+
+            /**
+            * Pipe standard IO streams between CSH Client and LifeCycle Adapter
+            */
             this.communicationHandler.pipeStdio();
 
+            /**
+            * LifeCycle Adapter runs Runner and starts Sequence in the container specified by provided configuration 
+            */
             await this.lifecycleAdapterMock.run(config);
 
         } catch (error) {
 
+            /**
+            * Make a container snapshot if snapshot was requested in LifeCycleConfig
+            */
             if (this.lifecycleConfig.makeSnapshotOnError) {
 
                 return await this.lifecycleAdapterMock.snapshot();
@@ -57,6 +111,9 @@ class LifeCycleController {
             return Promise.reject(new Error(error.message));
         }
 
+        /**
+        * LifeCycle Adapter performs cleanup operations including removing created volume, directory and container
+        */
         return this.lifecycleAdapterMock.cleanup();
     }
 }
--- packages/supervisor/src/test/lifecycle-controller-test.spec.ts
+++ packages/supervisor/src/test/lifecycle-controller-test.spec.ts
@@ -1,10 +1,7 @@
 import test from "ava";
-import * as sinon from "sinon";
 import { LifecycleDockerAdapterMock } from "@scramjet/supervisor/src/mocks/supervisor-component-mocks";
 import { LifeCycleController } from "@scramjet/supervisor/src/lib/lifecycle-controller";
 import { LifeCycleConfig } from "@scramjet/types/src/lifecycle";
-
-const sandbox = sinon.createSandbox();
 
 test("When an instance of LifeCycleController is construcred with correct parameters it must not be null", t => {
 
@@ -18,21 +15,3 @@ test("When an instance of LifeCycleController is construcred with correct parame
     t.not(controller, null);
 
 });
-
-test("When snapshot option is not false, snapshot method must not be called", t => {
-
-    const config: LifeCycleConfig = {
-        makeSnapshotOnError: false
-    };
-
-    const lcda = new LifecycleDockerAdapterMock();
-
-    const controller = new LifeCycleController(lcda, config);
-    let takeSnapshot = sandbox.spy(lcda, "snapshot");
-
-    controller.start();
-    t.false(takeSnapshot.calledOnce);
-    takeSnapshot.restore();
-
-});
-
