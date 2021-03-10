1457935362d73d9908a32bdfa93ba5c0781f793a
stokowski <rafal@scramjet.org>
2021-03-10 01:34:14 +0000

    simple error handling in prerunner script

--- packages/pre-runner/unpack-identify.sh
+++ packages/pre-runner/unpack-identify.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-tar zxf - -C /package && cat /package/package.json | \
-    jq -c '{ engines, image: .scramjet.image }'
+
+pkg_dir="/package"
+pkg_file="package.json"
+
+die() {
+    local msg=$1
+    local code=${2-1}
+    echo >&2 -e "{\"error\":\"${msg}\"}"
+    exit ${code}
+}
+
+if [ ! -w "${pkg_dir}" ]; then
+  die "${pkg_dir} doesnt exist or is not writable."
+fi
+
+tar zxf - -C ${pkg_dir} || die "Invalid pkg tar.gz archive"
+
+if [ ! -r "${pkg_dir}/${pkg_file}" ]; then
+  die "No ${pkg_file} in pkg archive."
+fi
+
+cat ${pkg_dir}/${pkg_file} \
+  | jq -c '{ engines, image: .scramjet.image }'
