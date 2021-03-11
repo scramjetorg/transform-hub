4e13db29de3b1ed8fdc633be92a4970c332cbb04
Ubuntu <at@scramjet.org>
2021-03-11 14:50:41 +0000

    Feature/test event in HelloAlice exapmle

--- packages/samples/example/src/index.ts
+++ packages/samples/example/src/index.ts
@@ -13,12 +13,19 @@ interface Person {
 // This method needs to expose  a function that will be executed by the runner.
 
 const mod: InertApp = function(input, ffrom, fto) {
+    this.on("test", () => console.error("Got test event")); // jeśli eventy będą odpowiednio podpięte w CShClient to powinniśmy zobaczyć ten event na consoli
+
     return fs.createReadStream(ffrom)
         .pipe(JSONStream.parse("*"))
         .pipe(new scramjet.DataStream())
         .map(
             (names: Person) => {
                 return `Hello ${names.name}! \n`;
+            }
+        )
+        .do(
+            (names: Person) => {
+                console.log(`Hello ${names.name}!`);
             }
         )
         .pipe(fs.createWriteStream(fto));
