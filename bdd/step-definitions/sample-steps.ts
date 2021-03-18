409bcdf2545ef83388e54d02af00a6b08b1b16f4
murbanowicz <mu@scramjet.org>
2021-03-18 14:05:45 +0000

    formatting changes

--- bdd/step-definitions/sample-steps.ts
+++ bdd/step-definitions/sample-steps.ts
@@ -7,16 +7,17 @@ import * as fs from "fs";
 const lineByLine = require("n-readlines");
 const testPath = "../dist/samples/example/";
 const supervisorExecutableFilePath = "../dist/supervisor/bin/supervisor.js";
+const packagePath = "../packages/pre-runner/sample-package/package.tar.gz";
 
 Given("input file containing data {string}", async (filename) => {
     assert.equal(await promisify(fs.exists)(`${testPath}${filename}`), true);
 });
 
 When("scramjet server porcesses input file as a stream", async () => {
-    process.env.SEQUENCE_PATH = '../packages/pre-runner/sample-package/package.tar.gz';
+    process.env.SEQUENCE_PATH = packagePath;
 
-    await new Promise(async (resolve, reject) => {
-        exec(`node ${supervisorExecutableFilePath} > dataOut.txt`, { timeout: 2000 }, (error, stdout, stderr) => {
+    await new Promise(async (resolve) => {
+        exec(`node ${supervisorExecutableFilePath} > dataOut.txt`, { timeout: 2000 }, (error) => {
             if (error) {
                 //kill sequence workaround to delete in the future
                 resolve(1);
@@ -34,11 +35,11 @@ Then("file {string} is generated", async (filename) => {
 Then("file {string} in each line contains {string} followed by name from file {string} finished by {string}", (file1, greeting, file2, suffix) => {
     const output = new lineByLine(`${file1}`);
 
-    let input = JSON.parse(fs.readFileSync(`${testPath}${file2}`, 'utf8'));
+    let input = JSON.parse(fs.readFileSync(`${testPath}${file2}`, "utf8"));
     let line1;
     let line2;
     let i = 0;
-    
+
     output.next();//skipp first line
     while ((line2 = output.next()) && (line1 = input[i++].name)) {
         assert.deepEqual(greeting + line1 + suffix, "" + line2);
