6ae9881e11b53475f9de1b62ebe66d33514724eb
Agnieszka Wypich <agne6ka@gmail.com>
2021-03-24 10:15:24 +0000

    Add vorpal to host-one.

--- packages/host-one/src/host-one.ts
+++ packages/host-one/src/host-one.ts
@@ -3,6 +3,7 @@ import { Server as HttpServer } from "http";
 import { EncodedMonitoringMessage } from "@scramjet/types";
 import { RunnerMessageCode, HandshakeAcknowledgeMessage, MessageUtilities } from "@scramjet/model";
 import { DataStream } from "scramjet";
+import * as vorpal from "vorpal";
 
 export class HostOne {
     // @ts-ignore    
@@ -19,6 +20,13 @@ export class HostOne {
     private sequencePath: string;
     // @ts-ignore
     private configPath: string;
+    // @ts-ignore
+    private vorpal: any;
+
+    errors = {
+        notNumber: "Provide a number",
+        emptyPath: "Path is empty"
+    }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     constructor(sequencePath: string, configPath: string){
@@ -31,6 +39,10 @@ export class HostOne {
         await this.startSupervisor(this.socketName);
         await this.createApiServer();
         await this.hookupMonitorStream();
+    }
+
+    init() {
+        this.vorpal = new vorpal();
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -92,4 +104,51 @@ export class HostOne {
         await this.controlStream.whenWrote(MessageUtilities.serializeMessage<RunnerMessageCode.PONG>(pongMsg));
     }
 
+    controlStreamsHandler() {
+        this.vorpal
+            .command("alive", "Confirm that sequence is alive when it is not responding")
+            .action(() => this.controlStream.whenWrote([RunnerMessageCode.ALIVE, {}]));
+
+        this.vorpal
+            .command("kill", "Kill forcefully sequence")
+            .action(() => this.kill());
+
+        this.vorpal
+            .command("stop", "Stop gracefully sequence")
+            /* 
+            * @feature/analysis-stop-kill-invocation
+            * Stop message must include two properties:
+            * timeout: number - the Sequence will be stopped after the provided timeout (miliseconds)
+            * canCallKeepalive: boolean - indicates whether Sequence can prolong operation to complete the task
+            * required for AppContext's:
+            * stopHandler?: (timeout: number, canCallKeepalive: boolean) => MaybePromise<void>;
+            * once the promise is resolved the Runner assumes it is safe to stop the Sequence
+            */
+            .action(() => this.controlStream.whenWrote([RunnerMessageCode.STOP, {}]));
+
+        this.vorpal
+            .command("event [EVENT_NAME] [JSON | ARRAY | ... ]", "Send event to the sequence")
+            .action((eventName: string, message: string) => {
+                // TODO: needs removal... why not JSON.parse?
+                let obj = eval(message); // temp eval
+
+                return this.controlStream.whenWrote([RunnerMessageCode.EVENT, { eventName, obj }]);
+            });
+
+        this.vorpal
+            .command("monitor [NUMBER]", "Change sequence monitoring rate")
+            .alias("rate")
+            .action((rate: string) => {
+                let rateNum = parseInt(rate, 10);
+
+                return isNaN(rateNum)
+                    ? this.vorpal.log(this.errors.notNumber)
+                    : this.controlStream.whenWrote([RunnerMessageCode.MONITORING_RATE, { rateNum }]);
+            });
+
+        this.vorpal
+            .delimiter("sequence:")
+            .show()
+            .parse(process.argv);
+    }
 }
