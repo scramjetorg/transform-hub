08ea6d01bf5605fee350b0968909ad00e35c3394
Agnieszka Wypich <agne6ka@gmail.com>
2021-03-25 15:27:55 +0000

    Refactor case with string.

--- packages/host-one/src/bin/start-host-one.ts
+++ packages/host-one/src/bin/start-host-one.ts
@@ -1,33 +1,53 @@
 #!/usr/bin/env node
 
-import { HostOne } from "../host-one";
+// import { HostOne } from "../host-one";
 import * as path from "path";
 import * as fs from "fs";
 
-let sequencePath: string = path.resolve(process.cwd(), process.argv[2]) || "";
-let configPath: string = path.resolve(process.cwd(), process.argv[3]) || "";
-let cmdArgs = process.argv[4] ? process.argv.slice(4) : "";
-let packageStrem;
-let appConfig;
-let sequenceArgs;
+// Example of usage: cat packg.tgz | ssh cz@woking scramjet - host - one "$(cat YourInputFile.txt)" true 1 abc ( todo:remove )
+let sequencePath: any;
+let configPath: any;
+let sequenceArgs: string | Array<any>;
+let packageStrem: any;
+let appConfig: any;
 
-if (!fs.existsSync(configPath)) {
-    console.error("Incorrect run argument: config path (" + configPath + ") does not exists. ");
-    process.exit(1);
-} else {
-    appConfig = fs.readFileSync(configPath);
-    console.log(appConfig);
+const errors = {
+    incorrectArg: "Incorrect argument:",
+    noPath: "Path does not exists:",
+    noData: "No data provided.",
+    missing: "Missing argument: "
 }
 
-if (!fs.existsSync(sequencePath)) {
-    console.error("Incorrect run argument: sequence path (" + sequencePath + ") does not exists. ");
-    process.exit(1);
-} else {
-    console.log(sequencePath);
-    packageStrem = fs.createReadStream(sequencePath);
+switch (true) {
+    case process.argv[2] === undefined:
+        console.error(errors.missing + 'sequence.');
+        process.exit(1);
+    case process.argv[3] === undefined:
+        console.error(errors.missing + 'config.');
+        process.exit(1);
+    default:
+        console.log('Checking data');
 }
 
-console.log(cmdArgs);
+sequenceArgs = process.argv[4] ? process.argv.slice(4) : "";
+
+// when string is passed
+const checkIfPathExist: any = (arg: string, cb: any) => {
+    let pathToFile = path.resolve(process.cwd(), arg)
+
+    if (!fs.existsSync(pathToFile)) {
+        console.error(`${errors.incorrectArg} ${arg}. ${errors.noPath} ${pathToFile}`);
+        process.exit(1);
+    } else {
+        return cb(pathToFile)
+    }
+}
+
+appConfig = checkIfPathExist(process.argv[2], (arg: string) => fs.readFileSync(arg));
+packageStrem = checkIfPathExist(process.argv[3], (arg: string) => fs.createReadStream(arg));
+
+console.log('appConfig', appConfig)
+console.log('packageStrem', appConfig)
 
 /**
 * Start hostOne script.
@@ -35,12 +55,11 @@ console.log(cmdArgs);
 * * Creates stream form sequence.
 * * Runs the host.
 */
-const hostOne: HostOne = new HostOne();
+// const hostOne: HostOne = new HostOne();
 
 /**
- * 
  * @param { stream } packageStrem - sequence stream
  * @param { object } appConfig - config file
  * @param { Array<any> } sequenceArgs - other optional arguments
  */
-hostOne.init(packageStrem, appConfig, sequenceArgs);
+// hostOne.init(packageStrem, appConfig, sequenceArgs);
