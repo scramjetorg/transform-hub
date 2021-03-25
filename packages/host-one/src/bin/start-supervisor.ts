6531f15e44466c084dca3712024a771bcf0b9c09
Ubuntu <alicja.gruzdz@scramjet.org>
2021-03-25 16:10:12 +0000

    Function that starts a Supervisor subprocess(spawn)

---
+++ packages/host-one/src/bin/start-supervisor.ts
@@ -0,0 +1,27 @@
+import { spawn } from "child_process";
+import { join } from "path";
+
+async function startSupervisor(socketPath: string) {
+
+    if (!socketPath) {
+        return Promise.reject(new Error("The path to socket is incorrect: " + socketPath));
+    }
+
+    const path = join(__dirname, "..", "..", "supervisor", "bin", "supervisor.js");
+    const command: string[] = ["node", path, socketPath];
+    const supervisor = spawn("/usr/bin/env", command);
+
+    supervisor.on("error", function(err) {
+        // What do we want to do if a Supervisor child process emits error event?
+        console.error("Supervisor process " + supervisor.pid + " threw an error: " + err);
+    });
+
+    supervisor.on("exit", function(code: any, signal: any) {
+        // Do we want to handle the Supervisor exit event and if so, how?
+        console.log("Supervisor process exited with code: " + code + ", signal: " + signal);
+    });
+
+    return Promise.resolve(supervisor.pid);
+}
+
+export { startSupervisor };
