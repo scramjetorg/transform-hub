8a3fa0c2faf8ab3c6ca5d598c95a9ae00d9039be
Agnieszka Wypich <agne6ka@gmail.com>
2021-03-25 13:12:29 +0000

    Change start host implementation.
    Fix linter.

--- packages/host-one/src/bin/start-host-one.ts
+++ packages/host-one/src/bin/start-host-one.ts
@@ -6,31 +6,41 @@ import * as fs from "fs";
 
 let sequencePath: string = path.resolve(process.cwd(), process.argv[2]) || "";
 let configPath: string = path.resolve(process.cwd(), process.argv[3]) || "";
+let cmdArgs = process.argv[4] ? process.argv.slice(4) : "";
+let packageStrem;
+let appConfig;
+let sequenceArgs;
 
 if (!fs.existsSync(configPath)) {
     console.error("Incorrect run argument: config path (" + configPath + ") does not exists. ");
     process.exit(1);
+} else {
+    appConfig = fs.readFileSync(configPath);
+    console.log(appConfig);
 }
 
 if (!fs.existsSync(sequencePath)) {
     console.error("Incorrect run argument: sequence path (" + sequencePath + ") does not exists. ");
     process.exit(1);
+} else {
+    console.log(sequencePath);
+    packageStrem = fs.createReadStream(sequencePath);
 }
+
+console.log(cmdArgs);
 
 /**
 * Start hostOne script.
-*
 * * Creates an instance of a hostOne.
+* * Creates stream form sequence.
 * * Runs the host.
-*
-* @param sequencePath - sequence file path
-* @param configPath - config file path
 */
-
-//TODO an array of strings - sequenceArguments can be passed as a third argument 
-//like here:["../../package/data.json", "out.txt"];
-//to discuss what would be the best solution? 
-//to avoid ./start-host-one sequencePath configPath seqArg1 seqArg2 ...
 const hostOne: HostOne = new HostOne();
 
-hostOne.main();
+/**
+ * 
+ * @param { stream } packageStrem - sequence stream
+ * @param { object } appConfig - config file
+ * @param { Array<any> } sequenceArgs - other optional arguments
+ */
+hostOne.init(packageStrem, appConfig, sequenceArgs);
