aa1facf0963e43c53bd76f21a47b826e9a0b55dc
Budleigh Salterton <cz@scramjet.org>
2021-03-26 15:06:57 +0000

    Better output checking

--- packages/api-server/src/get-handler.ts
+++ packages/api-server/src/get-handler.ts
@@ -1,18 +1,52 @@
-import { SequentialCeroRouter } from "./definitions";
+import { CeroError, NextCallback, SequentialCeroRouter } from "./definitions";
 import { MonitoringMessageCode } from "@scramjet/types";
 import { CommunicationHandler, MessageDataType } from "@scramjet/model";
+import { IncomingMessage, ServerResponse } from "node:http";
+import { mimeAccepts } from "./mime";
+import { getObject } from "./data-extractors";
 
 export function createGetterHandler(router: SequentialCeroRouter) {
-    return <T extends MonitoringMessageCode>(_path: string|RegExp, _op: T, _conn: CommunicationHandler): void => {
+    const check = (req: IncomingMessage): void => {
+        if (req.headers.accept) mimeAccepts(req.headers.accept, ["application/json", "text/json"]);
+    };
+    const output = (data: object, req: IncomingMessage, res: ServerResponse, next: NextCallback) => {
+        try {
+            const out = JSON.stringify(data);
+
+            res.writeHead(200, "OK", {
+                "content-type": "application/json"
+            });
+            return res.end(out);
+        } catch (e) {
+            return next(new CeroError("ERR_FAILED_TO_SERIALIZE", e));
+        }
+    };
+    /**
+     * Simple GET request hook for static data in monitoring stream.
+     *
+     * @param path the request path as string or regex
+     * @param op which operation
+     * @param conn the communication handler to use
+     */
+    const get = <T extends MonitoringMessageCode>(path: string|RegExp, op: T, conn: CommunicationHandler): void => {
         let lastItem: MessageDataType<T>|null = null;
 
-        _conn.addMonitoringHandler(_op, (data) => {
+        conn.addMonitoringHandler(op, (data) => {
             lastItem = data[1];
             return data;
         });
 
-        router.get(_path, (req, res) => {
-            res.end(lastItem);
+        router.get(path, async (req, res, next) => {
+            try {
+                check(req);
+                const data = await getObject(lastItem, req);
+
+                return output(data, req, res, next);
+            } catch (e) {
+                return next(new CeroError("ERR_INTERNAL_ERROR", e));
+            }
         });
     };
+
+    return get;
 }
