d14ab1cef8d291d31fb6489b8869deca4ca474e7
murbanowicz <mu@scramjet.org>
2021-03-29 10:47:19 +0000

    runner formatting

--- packages/runner/test/runner.spec.ts
+++ packages/runner/test/runner.spec.ts
@@ -1,44 +1,29 @@
 /* tslint:disable */
-// import { Runner } from "../src/runner";
-// import { FifoUtils } from "@scramjet/supervisor";
-// import { join } from "path";
-// import { DataStream } from "scramjet";
 import * as sinon from "sinon";
-// import { exec } from "child_process";
 import * as proxyquire from "proxyquire";
 import { PassThrough } from "stream";
 import { ReadStream, WriteStream } from "fs";
 import { RunnerMessageCode } from "../../types/node_modules/@scramjet/model/src";
-// import { RunnerMessageCode } from "../../types/node_modules/@scramjet/model/src";
-// import { StringStream } from "scramjet";
-
-// import { HandshakeAcknowledgeMessage, MessageUtilities, RunnerMessageCode } from "@scramjet/model";
-// import { RunnerMessageCode } from "@scramjet/model";
-// import { HandshakeAcknowledgeMessage, MessageUtilities, RunnerMessageCode } from "@scramjet/model";
-// const sandbox = sinon.createSandbox();
 
 const controlMockStream = new PassThrough() as unknown as ReadStream;
 const monitorMockStream = new PassThrough() as unknown as WriteStream;
-// const monitorMockStream = new Stream.Writable();
-
 const createReadStreamStub = () => controlMockStream;
 const createWriteStreamStub = () => monitorMockStream;
-
 const { Runner } = proxyquire("../dist/runner.js", {
     fs: {
         createReadStream: createReadStreamStub,
         createWriteStream: createWriteStreamStub,
     }
 });
-
 const test = require("ava");
 
 test("Runner new instance", (t: any) => {
     let runner = new Runner("sequencePath", ".");
+   
     t.not(runner, null);
 });
 
-test("Run main", async (t: any) =>  {
+test("Run main", async (t: any) => {
     let runner = new Runner("sequencePath", "fifoDir");
 
     const hookupFifoStreams = sinon.stub(runner, "hookupFifoStreams");
@@ -50,9 +35,9 @@ test("Run main", async (t: any) =>  {
     t.is(sendHandshakeMessage.callCount, 1);
 });
 
-test("Kill runner", async (t: any) =>  {
+test("Kill runner", async (t: any) => {
     let runner = new Runner("sequencePath", "fifoDir");
- 
+
     const processExit = sinon.stub(process, "exit");
     const cleanupControlStreamMock = sinon.stub(runner, "cleanupControlStream");
 
@@ -64,18 +49,16 @@ test("Kill runner", async (t: any) =>  {
         }, 500);
     });
 
-    t.is(cleanupControlStreamMock.callCount, 1);  
-    t.is(processExit.callCount, 1);  
+    t.is(cleanupControlStreamMock.callCount, 1);
+    t.is(processExit.callCount, 1);
 });
-
-
 
 // test("Kill runner", async (t: any) =>  {
 //     let runner = new Runner("sequencePath", "fifoDir");
 
 //     // const runnerKill = sinon.stub(runner, "handleKillRequest");
 //     // const processExit = sinon.spy(process, "exit");
-    
+
 //     // const hookupFifoStreams = sinon.stub(runner, "hookupFifoStreams");
 //     const sendHandshakeMessage = sinon.stub(runner, "sendHandshakeMessage");
 
@@ -95,7 +78,7 @@ test("Kill runner", async (t: any) =>  {
 //     //     await t.is(processExit.callCount, 0);
 //     // }, 500).unref();
 
-    
+
 //     // await new Promise(async (resolve) => {
 //     //     setTimeout(() => {
 //     //         // await StringStream.from(await StringStream.from(controlMockStream).whenWrote([RunnerMessageCode.KILL, {}]);
@@ -119,7 +102,7 @@ test("Kill runner", async (t: any) =>  {
 
 //     const hookupFifoStreams = sinon.stub(runner, "hookupFifoStreams");
 //     const sendHandshakeMessage = sinon.stub(runner, "sendHandshakeMessage");
-        
+
 //     await runner.main();
 
 //     t.is(hookupFifoStreams.callCount, 1);
