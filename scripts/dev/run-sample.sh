e8c6b7e88a25a43740a447c75807377f500ebd34
Budleigh Salterton <cz@scramjet.org>
2021-04-27 20:48:37 +0000

    Fix run sample script

--- scripts/dev/run-sample.sh
+++ scripts/dev/run-sample.sh
@@ -17,7 +17,7 @@ cd $(git rev-parse --show-toplevel)
 yarn install
 
 # build example to dist
-lerna run build
+yarn bic
 
 if [ "$DOCKER" = true ] ; then
     lerna run build:docker
@@ -30,8 +30,18 @@ cd packages/pre-runner
 yarn prepare-sample-tar
 
 # copy to dist
-lerna run prepack
+if [ "$INSTALL" = true ] ; then
+    lerna run prepack
+    yarn prepare-sample-tar
+fi
 
 # start hostOne - package.json simulates config file
-cd $(git rev-parse --show-toplevel)
-node dist/host-one/bin/start-host-one.js packages/pre-runner/sample-package/package.tar.gz package.json /package/data.json output.txt
+if [ "$TS" = true ] ; then
+
+    cd $(git rev-parse --show-toplevel)/packages/host-one/src/bin
+    ts-node start-host-one.ts ../../../pre-runner/sample-package/package.tar.gz ../../package.json /package/data.json output.txt
+else
+    cd $(git rev-parse --show-toplevel)
+    node dist/host-one/bin/start-host-one.js packages/pre-runner/sample-package/package.tar.gz package.json /package/data.json output.txt
+fi
+
