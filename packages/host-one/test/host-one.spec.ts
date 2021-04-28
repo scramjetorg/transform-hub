db527c194dffcb2950d4d9385277add1d5aa0e37
Agnieszka Wypich <agne6ka@gmail.com>
2021-04-28 09:39:49 +0200

    Delete supervisor socket test.

--- packages/host-one/test/host-one.spec.ts
+++ packages/host-one/test/host-one.spec.ts
@@ -200,3 +200,37 @@ test("Vorpal should execute controlStream when command monitor is provided", asy
     t.is(conStream.getCall(0).firstArg[0], 4003);
     t.deepEqual(conStream.getCall(0).firstArg[1], { monitoringRate: 200 });
 });
+
+
+/*
+* Supervisor process starting will be also tested as a part of BDD test.
+* ToDo: Test supervisor exit.
+*/
+
+/*
+test("start supervisor on " + testSocket, async t => {
+    const child = spawn("/usr/bin/env", ["node", "./dist/bin/supervisor.js", testSocket]);
+
+    await new Promise(async (resolve) => {
+        setTimeout(() => {
+            spawn("/usr/bin/env", ["rm", testSocket]);
+            child.kill(9);
+        }, 8000);
+
+        const data = await StringStream.from(child.stdout).lines().slice(0, 1).whenRead();
+
+        t.is(data, `listening on socket ${testSocket}`);
+
+        child.on("exit", function(code: any, signal: any) {
+            if (code === 1) {
+                t.fail("child process exited with " +
+                    `code ${code} and signal ${signal}`);
+            } else if (code === 0 || signal === "SIGKILL") {
+                t.pass();
+            }
+
+            resolve(1);
+        });
+    });
+});
+*/
