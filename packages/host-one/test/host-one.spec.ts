0bbe073f26209ec8465b8ed754c89504e1c77f98
Ubuntu <alicja.gruzdz@scramjet.org>
2021-05-04 12:47:19 +0000

    Fix host-one vorpal test: message EVENT changed number

--- packages/host-one/test/host-one.spec.ts
+++ packages/host-one/test/host-one.spec.ts
@@ -43,7 +43,7 @@ test("Vorpal should not execute controlStream when params in 'event' command are
 
 test("Vorpal should execute a controlStream with proper format, when user enter an 'event' command", async t => {
     /**
-     * controlStream param should be [4005, { eventName: 'string', message: 'any'}]
+     * controlStream param should be [5001, { eventName: 'string', message: 'any'}]
      */
     const hostOne = new HostOne();
 
@@ -56,7 +56,7 @@ test("Vorpal should execute a controlStream with proper format, when user enter 
     hostOne.controlStreamsCliHandler();
     await hostOne.vorpalExec("event fooBar ['bar']");
     t.true(conStream.getCall(0).firstArg instanceof Array);
-    t.is(conStream.getCall(0).firstArg[0], 4005);
+    t.is(conStream.getCall(0).firstArg[0], 5001);
     t.is(typeof conStream.getCall(0).firstArg[1], "object");
     t.is(typeof conStream.getCall(0).firstArg[1].eventName, "string");
     t.not(conStream.getCall(0).firstArg[1].message, null);
@@ -86,7 +86,7 @@ test("when command 'event' with event name and message as a number is provided, 
     hostOne.controlStreamsCliHandler();
     await hostOne.vorpalExec("event fooBar 5");
     t.true(conStream.getCall(0).firstArg instanceof Array);
-    t.is(conStream.getCall(0).firstArg[0], 4005);
+    t.is(conStream.getCall(0).firstArg[0], 5001);
     t.is(typeof conStream.getCall(0).firstArg[1].eventName, "string");
     t.is(typeof conStream.getCall(0).firstArg[1].message, "number");
 });
@@ -103,7 +103,7 @@ test("when command 'event' with event name and message as an array is provided, 
     hostOne.controlStreamsCliHandler();
     await hostOne.vorpalExec("event fooBar ['dada']");
 
-    t.is(conStream.getCall(0).firstArg[0], 4005);
+    t.is(conStream.getCall(0).firstArg[0], 5001);
     t.is(typeof conStream.getCall(0).firstArg[1].eventName, "string");
     t.true(conStream.getCall(0).firstArg[1].message instanceof Array);
 });
@@ -120,7 +120,7 @@ test("when command 'event' with event name and message as an obj is provided, Vo
     hostOne.controlStreamsCliHandler();
     await hostOne.vorpalExec("event fooBar ()=>{console.log('test')}");
 
-    t.is(conStream.getCall(0).firstArg[0], 4005);
+    t.is(conStream.getCall(0).firstArg[0], 5001);
     t.is(typeof conStream.getCall(0).firstArg[1].eventName, "string");
     t.is(typeof conStream.getCall(0).firstArg[1].message, "function");
 });
