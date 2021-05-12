9c38692c4f6a26ae725eb108f11db392c207b4ba
Ubuntu <alicja.gruzdz@scramjet.org>
2021-05-12 08:43:14 +0000

    Add Sequence type and comment

--- packages/host/src/lib/host.ts
+++ packages/host/src/lib/host.ts
@@ -5,33 +5,75 @@ import { Readable } from "stream";
 import * as os from "os";
 import * as path from "path";
 
+/**
+ * 
+ * Sequence type describes the collection
+ * of uncompressed developer's code files
+ * and the configuration file attached to them
+ * residing on certain volume.
+ * 
+ * The configuration file is required to run
+ * Sequence Instance.
+ * 
+ * Question: this should probably moved to @scramjet/model, right?
+ * 
+ */
+export type Sequence = {
+
+    id: string,
+    config: RunnerConfig
+
+}
+
+/**
+ * 
+ * An utility class for manipulates the
+ * Sequences stored on the CSH.
+ * 
+ * Question: Patryk raised an issue that we should think of
+ * saving the Sequence information in the file (for the future sprints)
+ */
+export class SequenceStore {
+
+    sequences: { [key: string]: Sequence } = {}
+
+    getSequenceById(key: string): Sequence {
+        return this.sequences[key];
+    }
+
+    addSequence(sequence: Sequence) {
+        if (sequence)
+            this.sequences[sequence.id] = sequence;
+    }
+
+    deleteSequence(id: string) {
+        /**
+         * TODO: Here we also need to check if there aren't any Instances running 
+         * that use this Sequence.
+         */
+        if (id)
+            delete this.sequences[id];
+    }
+}
+
 export class Host implements IComponent {
 
-    /**
-     * 
-     */
     apiServer: APIExpose;
 
-    /**
-     *
-     */
     csiControllers: { [key: string]: string } = {} // temp: the value type in the map will be a CSI Controller object
 
-    /**
-     *
-     */
-    sequences: { [key: string]: string } = {}
+    sequenceStore: SequenceStore = new SequenceStore();
 
     /**
      * SocketServer instance.
      */
-//    private socketServer?: SocketServer;
+    //    private socketServer?: SocketServer;
 
     /**
      * SocketServer path (address).
      * Supervisor connects to this path.
      */
-    private socketServerPath: string;
+   private socketServerPath: string;
 
     logger: Logger;
 
@@ -72,7 +114,7 @@ export class Host implements IComponent {
         process.on("beforeExit", () => {
             this.apiServer?.server?.close();
         });
-
+        // Question: how to connect the general Host-related API endpoints? Do they require modifications in API server?
         //        this.apiServer.get(`${apiBase}/instances`, RunnerMessageCode.MONITORING, this.communicationHandler);
         //        this.apiServer.get(`${apiBase}/sequences`, RunnerMessageCode.MONITORING, this.communicationHandler);
 
@@ -96,12 +138,12 @@ export class Host implements IComponent {
         throw new Error("Method not yet supported");
     }
 
-    getSequencesMap(): { [key: string]: string } {
-        return this.sequences;
-    }
-
     getCSIControllersMap(): { [key: string]: string } {
         return this.csiControllers;
+    }
+
+    getSequencesMap(): { [key: string]: Sequence } {
+        return this.sequenceStore.sequences;
     }
 }
 
