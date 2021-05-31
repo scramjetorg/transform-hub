68adef883017ec0b93a34b64eac25e21e162b15f
Ubuntu <alicja.gruzdz@scramjet.org>
2021-05-31 08:32:48 +0000

    App corrected

--- packages/reference-apps/checksum-sequence/index.ts
+++ packages/reference-apps/checksum-sequence/index.ts
@@ -1,26 +1,19 @@
 /* eslint-disable no-loop-func */
 var crypto = require("crypto");
+import { TransformApp } from "@scramjet/types";
 
-const exp = [
-    /**
-     * @param _stream - input
-     * @returns data
-     */
-    async function(_stream: any) {
+export = async function(_stream: any) {
 
-        let chunks = "";
+    let chunks = "";
 
-        for await (const chunk of _stream) {
-            console.log("----------------------- chunk: " + chunk);
-            chunks += chunk.toString();
-        }
-
-        const hex = crypto.createHash("md5").update(chunks).digest("hex");
-
-        return hex.toString();
-
-
+    for await (const chunk of _stream) {
+        console.log("----------------------- chunk: " + chunk);
+        chunks += chunk.toString();
     }
-];
 
-export = exp;
+    const hex = crypto.createHash("md5").update(chunks).digest("hex");
+
+    return hex.toString();
+
+} as TransformApp;
+
