bfecedd74e0beb9e30037f4cabf52e8b62292e8b
kmagonski <kamil.magonski@scramjet.com>
2021-06-10 11:28:10 +0200

    add image config test
    fix
    
    fix2
    
    Update packages/csi-config/test/index.spec.ts
    
    Co-authored-by: Patryk "Patu" Falba <patuwwy@gmail.com>

--- packages/csi-config/test/index.spec.ts
+++ packages/csi-config/test/index.spec.ts
@@ -14,4 +14,18 @@ test("Check the imageConfig test", async t => {
     t.is(typeof imageConfig, "object", "Imageconfig is an object");
     t.is(typeof imageConfig.prerunner, "string", "Exposes prerunner");
     t.is(typeof imageConfig.runner, "string", "Exposes runner");
+
+});
+
+test("Check if the tags of the images match packages version", async t => {
+    const runnerPackageJson = require("../../runner/package.json");
+    const preRunnerPackageJson = require("../../pre-runner/package.json");
+    const imageConfig = await config.imageConfig();
+    const runnerTagImageConfig = imageConfig.runner.split(":")[1];
+    const preRunnerTagImageConfig = imageConfig.prerunner.split(":")[1];
+    const runnerTagPackageJson = runnerPackageJson.version;
+    const preRunnerTagPackageJson = preRunnerPackageJson.version;
+
+    t.is(runnerTagPackageJson, runnerTagImageConfig, "Runner tag is eqal");
+    t.is(preRunnerTagPackageJson, preRunnerTagImageConfig, "Prerunner tag is eqal");
 });
