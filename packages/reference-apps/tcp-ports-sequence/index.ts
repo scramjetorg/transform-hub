0fee3f64e18c32737a0b786c8e3c729081508677
Ubuntu <alicja.gruzdz@scramjet.org>
2021-06-11 10:52:20 +0000

    still with console.logs

--- packages/reference-apps/tcp-ports-sequence/index.ts
+++ packages/reference-apps/tcp-ports-sequence/index.ts
@@ -1,4 +1,4 @@
-import * as http from "http";
+import * as http from "https";
 
 import { ReadableApp } from "@scramjet/types";
 import { PassThrough } from "stream";
@@ -16,24 +16,20 @@ const createServers = (output: PassThrough): http.Server[] => {
             console.log("~~~~~~~~~~~~~~~Server closed.");
         });
         server.on("error", function(error) {
-            console.log("~~~~~~~~~~~~~~~Server error: " + error);
+            console.error("~~~~~~~~~~~~~~~Server error: " + error);
         });
 
         server.on("connection", function(socket) {
 
-            console.log("~~~~~~~~~~~~~~~ Socket is listening at port" + socket.remotePort);
-            console.log("~~~~~~~~~~~~~~~ Socket ip :" + socket.remoteAddress);
+            console.log("~~~~~~~~~~~~~~~ Socket is listening at port: " + socket.remotePort);
 
             socket.on("data", function(data: any) {
                 console.log("~~~~~~~~~~~~~~~Data sent to server and output: " + data);
-
                 output.write(data);
-                //                output.end();
-
             });
 
             socket.on("error", function(error: any) {
-                console.log("~~~~~~~~~~~~~~~Error: " + error);
+                console.error("~~~~~~~~~~~~~~~Socket Error: " + error);
             });
 
             socket.on("end", function(data: any) {
@@ -42,22 +38,35 @@ const createServers = (output: PassThrough): http.Server[] => {
             });
 
             socket.on("close", function(error: any) {
-                console.log("~~~~~~~~~~~~~~~Socket closed.");
+                console.log("~~~~~~~~~~~~~~~Socket closed, error: " + error);
                 output.end();
-                console.error(error);
             });
-
 
         });
 
         server.listen(port, () => {
             console.log("~~~~~~~~~~~~~~~ Listening at port: " + port);
-
         });
+
         servers.push(server);
     });
 
     return servers;
+};
+const testServerConnection = (portNumber: number) => {
+
+    const net = require("net");
+    const client = new net.Socket();
+
+    client.connect({
+        port: portNumber
+    });
+
+    client.on("connect", function() {
+        console.log("~~~~~~~~~~~~~~~ Client: connection established");
+        client.write("Is there anybody out there?");
+    });
+
 };
 /**
  * @param _stream - input
@@ -67,6 +76,10 @@ const mod: ReadableApp = async function(_stream: any) {
     const output = new PassThrough();
 
     createServers(output);
+
+    await new Promise(res => setTimeout(res, 10000));
+
+    testServerConnection(7006);
 
     return output;
 };
