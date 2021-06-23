fbe8a9c23d2678db898f41ee854b4e16e1a31a21
Budleigh Salterton <cz@scramjet.org>
2021-06-23 11:38:52 +0000

    Minor fixes

--- packages/sth/src/bin/hub.ts
+++ packages/sth/src/bin/hub.ts
@@ -1,5 +1,4 @@
 import { Command } from "commander";
-import { startHost } from "@scramjet/host";
 // import { version } from "../../package.json";
 
 const program: Command = new Command() as Command;
@@ -12,10 +11,14 @@ const options = program
     .opts()
 ;
 
-startHost({}, options.socketLocation as string, {
+// here we would do config.write(option, value);
+
+// before here we actually load the host and we have the config imported elsewhere
+// so the config is changed before compile time, not in runtime.
+require("@scramjet/host").startHost({}, options.socketLocation as string, {
     identifyExisting: options.identifyExisting as boolean
 })
-    .catch(e => {
+    .catch((e: Error & { exitCode?: number }) => {
         console.error(e.stack);
         process.exitCode = e.exitCode || 1;
         process.exit();
