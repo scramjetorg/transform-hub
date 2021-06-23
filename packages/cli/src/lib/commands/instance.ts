90b6ea3a3901d621b098e43a797f57abe8a125dd
Agnieszka Wypich <agne6ka@gmail.com>
2021-06-23 13:09:48 +0000

    Update instance commands.

--- packages/cli/src/lib/commands/instance.ts
+++ packages/cli/src/lib/commands/instance.ts
@@ -5,29 +5,76 @@ export const instance: CommandDefinition = (program) => {
     const instanceCmd = program
         .command("instance [command]")
         .alias("inst")
-        .description("operations on instance, see what is instance: <URL>");
+        .description("operations on instance");
 
     instanceCmd.command("kill <id>")
-        .description("kill the instance")
+        .description("kill instance without waiting for unfinished tasks")
         .action((id) => {
             console.log("Instance id ", id);
         });
 
     instanceCmd.command("stop <id>")
-        .description("stop the instance")
+        .description("end instance gracefully waiting for unfinished tasks")
         .action((id) => {
             console.log("Instance id ", id);
         });
 
-    instanceCmd.command("get <id>")
-        .description("get data about the instance")
+    instanceCmd.command("status <id>")
+        .description("status data about the instance")
         .action((id) => {
             console.log("Instance id ", id);
         });
 
-    instanceCmd.command("delete <id>")
-        .alias("rm")
-        .description("delete the instance")
+    instanceCmd.command("health <id>")
+        .description("show the instance health status")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("info <id>")
+        .description("show info about the instance")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("sendEvent <id> <json>")
+        .description("send event with eventName and object|array|function|number")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("event <id> <json>")
+        .description("invoke the event by eventName and optionally with message")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("input <id>")
+        .description("send stream to input")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("output <id>")
+        .description("show stream on output")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("stdout <id>")
+        .description("show stream on stdout")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("stdin <id>")
+        .description("send stream to stdin")
+        .action((id) => {
+            console.log("Instance id ", id);
+        });
+
+    instanceCmd.command("stderr <id>")
+        .description("show stream on stderr")
         .action((id) => {
             console.log("Instance id ", id);
         });
