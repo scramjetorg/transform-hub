f5799b518ac4fc7c74487fbfa8dfa99ad350cbfa
Budleigh Salterton <cz@scramjet.org>
2021-06-25 15:10:34 +0000

    Include new refapps

--- scripts/add-to-packages-json.js
+++ scripts/add-to-packages-json.js
@@ -4,33 +4,39 @@ const { glob } = require("glob");
 const { readFileSync, writeFileSync } = require("fs");
 const { resolve } = require("path");
 const cwd = resolve(__dirname, "../");
-const packages = glob.sync("packages/**/package.json", {
-    cwd,
-    ignore: "**/node_modules/**"
-});
 const options = {};
-const [path, value] = process.argv.slice(2).reduce((acc, x) => {
+const [path, _value, match] = process.argv.slice(2).reduce((acc, x) => {
     if (x.startsWith("--")) options[x.substr(2)] = true;
     else if (x.startsWith("-")) x.substr(1).split("").forEach(y => { options[y] = true; });
     else acc.push(x);
     return acc;
 }, []);
-// const [,, path, value, ...opts] = process.argv;
-const chunks = path.split(".");
-const key = chunks.pop();
 
 if (!path || options.help || options.h || options["?"]) {
-    console.error(`Usage: ${process.argv[1]} [--override|-o] <key-path> [<value>]`);
+    console.error(`Usage: ${process.argv[1]} [--delete|-d] [--override|-o] <key-path> [<value>] [<name-match>]`);
     process.exit(1);
 }
 
+const packages = glob.sync("packages/**/package.json", {
+    cwd,
+    ignore: "**/node_modules/**"
+});
+const chunks = path.split(".");
+const key = chunks.pop();
+const re = match ? require("globrex")(match).regex : "";
+const del = options.d || options.delete;
+const override = del || options.override || options.o;
+const value = del ? undefined : _value;
+
 for (const file of packages) {
+    if (!file.match(re)) continue;
+
     try {
         const str = readFileSync(resolve(cwd, file), { encoding: "utf-8" });
         // eslint-disable-next-line import/no-dynamic-require
         const contents = JSON.parse(str);
 
-        if (value) {
+        if (del || value) {
             const obj2 = chunks.reduce((acc, k) => {
                 if (typeof acc[k] === "object") return acc[k];
 
@@ -40,8 +46,8 @@ for (const file of packages) {
                 return acc[k];
             }, contents);
 
-            if (obj2 && typeof obj2 === "object") {
-                if (!(key in obj2) || options.override || options.o) {
+            if (typeof obj2 === "object") {
+                if ((!(key in obj2) || override) && obj2[key] !== value) {
                     obj2[key] = value;
                     writeFileSync(resolve(cwd, file), JSON.stringify(contents, null, 2), "utf-8");
                 }
