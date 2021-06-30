e7c7a02d5a8ccd9eadfe4b825636064a6f87083d
patuwwy <patuwwy@gmail.com>
2021-06-30 16:14:38 +0000

    Test if Hub exposes Host's API on port provided with params

---
+++ bdd/step-definitions/hub/config-port.ts
@@ -0,0 +1,57 @@
+import { Then, When } from "@cucumber/cucumber";
+import { CustomWorld } from "../world";
+
+import { HostClient } from "@scramjet/api-client";
+
+import { strict as assert } from "assert";
+import { ChildProcess, spawn } from "child_process";
+import path = require("path");
+import { SIGTERM } from "constants";
+
+When("hub process is started with parameters {string}", async function(this: CustomWorld, params: string) {
+    await new Promise<void>(async (resolve, reject) => {
+        this.resources.hub = spawn(
+            "node", [path.resolve(__dirname, "../../../dist/sth/bin/hub"), ...params.split(" ")],
+            {
+                detached: true
+            }
+        );
+
+        if (process.env.SCRAMJET_TEST_LOG) {
+            this.resources.hub?.stdout?.pipe(process.stdout);
+            this.resources.hub?.stderr?.pipe(process.stderr);
+        }
+
+        this.resources.hub.on("error", reject);
+
+        const chunks = [];
+
+        for await (const chunk of this.resources.hub.stdout) {
+            chunks.push(chunk);
+
+            if (~chunks.join("").indexOf("API listening on port")) {
+                break;
+            }
+        }
+
+        resolve();
+    });
+});
+
+Then("API is available on port {int}", async function(this: CustomWorld, port: number) {
+    const hostClient = new HostClient(`http://127.0.0.1:${port}/api/v1`);
+    const status = (await hostClient.getVersion()).status;
+
+    assert.equal(status, 200);
+});
+
+Then("exit hub process", async function(this: CustomWorld) {
+    await new Promise<void>(async (resolve) => {
+        const hub = this.resources.hub as ChildProcess;
+
+        hub.on("close", resolve);
+
+        hub.kill(SIGTERM);
+    });
+});
+
