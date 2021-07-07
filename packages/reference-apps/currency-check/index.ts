d1d75c8d4f967cd7c1ccdf7eaaab374fb636b6bd
patuwwy <patuwwy@gmail.com>
2021-07-07 23:04:10 +0000

    Change to coinbase api

--- packages/reference-apps/currency-check/index.ts
+++ packages/reference-apps/currency-check/index.ts
@@ -1,20 +1,14 @@
 import { ReadableApp } from "@scramjet/types";
 import { PassThrough } from "stream";
 import * as https from "https";
-import { StringDecoder } from "string_decoder";
-
-let API_KEY: string;
 
 const getData = async (pair: string) => {
     return new Promise<any>(resolve => {
-        https.get(`https://free.currconv.com/api/v7/convert?q=${pair}&compact=ultra&apiKey=${API_KEY}`, async (res) => {
-            const decoder = new StringDecoder();
+        https.get(`https://api.coinbase.com/v2/prices/${pair}/spot`, async (res) => {
+            const chunks: Buffer[] = [];
 
-            for await (const d of res) {
-                decoder.write(d);
-            }
-
-            resolve(decoder.end());
+            res.on("data", (d) => chunks.push(d));
+            res.on("end", () => resolve(chunks.join("")));
         });
     });
 };
@@ -26,17 +20,16 @@ const getData = async (pair: string) => {
  */
 export = async function(_stream, ...args) {
     const ps = new PassThrough();
-    const currencyPair = [args[1], args[2]].join("_");
-
-    API_KEY = args[0];
+    const currencyPair = [args[0], args[1]].join("-");
 
     let cnt = 0;
 
     setInterval(async () => {
         this.logger.log(cnt);
 
+        ps.write(await getData(currencyPair) + "\r\n");
+
         if (cnt === 0) {
-            ps.write(await getData(currencyPair));
             cnt = 60;
         }
 
