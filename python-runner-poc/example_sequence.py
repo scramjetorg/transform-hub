c2922a4906ed961f87fb65915ce4e41925666f02
Jan Warchoł <jan.warchol@scramjet.org>
2021-07-21 18:07:03 +0200

    WIP use aiostream and process whole stream

--- python-runner-poc/example_sequence.py
+++ python-runner-poc/example_sequence.py
@@ -1,9 +1,24 @@
 import asyncio
+import json
+from datetime import datetime
+from aiostream import stream, pipe
 
-async def transform(data, context):
-    await asyncio.sleep(2)  # do some IO-bound processing
-    data['result'] = data['value']
-    if 'args' in context:
-        data['result'] += context['args']
-    del data['value']
-    return data
+def with_time(text):
+    print(f'{datetime.now().strftime("%H:%M:%S")} - {text}')
+
+async def transform(stream, context):
+    async def process(chunk):
+        data = json.loads(chunk)
+        with_time(f'Processing #{data["counter"]}')
+
+        await asyncio.sleep(2)
+        data['result'] = data['value']
+        if 'add' in context:
+            data['result'] += context['add']
+        del data['value']
+
+        with_time(f'Finished #{data["counter"]}')
+        return json.dumps(data) + '\n'
+
+    return stream | pipe.map(process)
+
