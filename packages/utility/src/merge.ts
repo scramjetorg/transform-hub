b521d7ab88ede376a1a0d3b74efb54e022468f90
patuwwy <patuwwy@gmail.com>
2021-07-22 10:44:16 +0000

    Fix merge util overwriting with undefined

--- packages/utility/src/merge.ts
+++ packages/utility/src/merge.ts
@@ -2,8 +2,9 @@ import { DeepPartial } from "@scramjet/types";
 
 export const merge = <T extends Record<string, unknown>>(objTo: T, objFrom: DeepPartial<T>) => Object.keys(objFrom)
     .forEach((key: keyof T) => {
-        if (typeof objFrom[key] === "object" && !Array.isArray(objFrom[key]))
+        if (typeof objFrom[key] === "object" && !Array.isArray(objFrom[key])) {
             merge(objTo[key] as Record<string, unknown>, objFrom[key]!);
-        else
+        } else if (objFrom[key]) {
             objTo[key] = objFrom[key] as T[keyof T];
+        }
     });
