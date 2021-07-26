1f2ba7ef417b8d492533f635ca3da3e8a3d7eb45
Budleigh Salterton <cz@scramjet.org>
2021-07-26 23:14:31 +0000

    v0.10.5

--- scripts/publish-order-dist-packages.js
+++ scripts/publish-order-dist-packages.js
@@ -4,10 +4,11 @@
 /* eslint-disable no-return-assign */
 
 const { exec: _exec } = require("child_process");
-const { readFile } = require("fs/promises");
+const { writeFile, readFile } = require("fs/promises");
 const { glob } = require("glob");
 const { resolve, dirname } = require("path");
 const { DataStream } = require("scramjet");
+const toposort = require("toposort");
 const { promisify } = require("util");
 const cwd = resolve(__dirname, "../dist/");
 const pkgs = glob.sync("./*/package.json", {
@@ -43,19 +44,20 @@ if (!repo && !main) {
     process.exit();
 }
 
-const checkIfPublished = async (repoSwitch, opts, item) => {
+const checkIfPublished = async (opts, item) => {
     try {
-        const out = await exec(`npm view ${repoSwitch} --json ${item.name}@${item.version}`, opts);
+        const out = await exec(`npm view --json ${item.name}@${item.version}`, opts);
 
         return out.stdout.length > 0;
     } catch (e) {
-        return true;
+        return false;
     }
 };
 
 // eslint-disable-next-line complexity
 (async () => {
     const files = await DataStream.from(pkgs)
+        .map((x) => resolve(cwd, x))
         .map(async (x) => [await readFile(x, "utf-8"), x])
         .map(([contents, file]) => ({ file, package: JSON.parse(contents) }))
         .map(({ file, package: { name, version, dependencies } }) => ({
@@ -75,53 +77,56 @@ const checkIfPublished = async (repoSwitch, opts, item) => {
             ? Object.keys(dependencies).filter(z => packages.includes(z))
             : []
     }));
-    const deps = filesWithDeps.reduce((acc, x) => {
-        x.deps?.forEach(n => acc[n] = (acc[n] || 0) + 1);
+    const graph = filesWithDeps.reduce((acc, x) => {
+        x.deps?.forEach(
+            (y) => acc.push([x.name, y])
+        );
         return acc;
-    }, {});
+    }, []);
+    const order = toposort(graph);
 
-    Object.entries(deps).forEach(([p, v]) => {
-        filesWithDeps.find(x => x.name === p).visits = v;
-    });
     filesWithDeps.sort((a, b) => {
-        if (a.deps.includes(b.name)) return 1;
-        if (b.deps.includes(a.name)) return -1;
-        return b.visits - a.visits - (a.deps.length - b.deps.length) / 1e6;
-    });
+        return order.indexOf(a.name) - order.indexOf(b.name);
+    }).reverse();
 
     for (const item of filesWithDeps) {
         try {
             const opts = { cwd: dirname(item.fullFile) };
-            const repoSwitch = local
-                ? `--repository="${repo.replace("\"", "\"'\"'\"")}"`
-                : "";
 
-            if (skip && await checkIfPublished(repoSwitch, opts, item)) {
+            if (dryRun) {
+                console.error("Would publish", item.name);
+                continue;
+            }
+
+            if (local) {
+                await writeFile(resolve(opts.cwd, ".npmrc"), `@scramjet:registry=${repo}\n`);
+            } else {
+                await writeFile(resolve(opts.cwd, ".npmrc"), "");
+            }
+
+            if (skip && await checkIfPublished(opts, item)) {
                 console.error(" ---> Found in npm: ", item.name, item.version);
                 continue;
             }
 
             console.error(" -=> Publishing", item.name);
-            await exec(`npm i --package-lock-only ${repoSwitch}`, opts);
+            await exec("npm i --package-lock-only", opts);
             console.error(" `-> created package-lock.");
-            if (dryRun)
-                await exec("echo would publish...", opts);
-            else {
-                await exec(`npm publish ${repoSwitch}`, opts);
-                console.error(" ... waiting until new package visible");
-                let tries = 120;
+            await exec("npm publish", opts);
+            console.error(" ... waiting until new package visible");
+            let tries = 120;
 
-                while (--tries > 0) {
-                    if (await checkIfPublished(repoSwitch, opts, item)) {
-                        break;
-                    }
-                    process.stderr.write(`\r ... still waiting, tries left: ${tries}`);
-                    await new Promise(res => setTimeout(res, 5000));
+            while (--tries > 0) {
+                if (await checkIfPublished(opts, item)) {
+                    break;
                 }
-                console.error("");
-                if (!tries)
-                    throw new Error("Package still not available after 600 secs");
+                process.stderr.write(`\r ... still waiting, tries left: ${tries}`);
+                await new Promise(res => setTimeout(res, 5000));
             }
+            console.error("");
+            if (!tries)
+                throw new Error("Package still not available after 600 secs");
+
             console.error(" `-> ops done, waiting until new package visible");
         } catch (e) {
             if (!force)
