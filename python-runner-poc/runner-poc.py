0b9da845c36079a5d8d380d92369db584819cde3
Jan Warchoł <jan.warchol@scramjet.org>
2021-07-27 17:02:42 +0200

    WIP add a simple appcontext with logging

--- python-runner-poc/runner-poc.py
+++ python-runner-poc/runner-poc.py
@@ -6,48 +6,66 @@ import asyncio
 import aiofiles
 from aiostream import stream, pipe
 import json
-from datetime import datetime
 import importlib
+import logging
 
 try:
     INPUT = sys.argv[1]
     OUTPUT = sys.argv[2]
     CONTROL = sys.argv[3]
-    MODULE = sys.argv[4]
+    LOGGING = sys.argv[4]
+    MODULE = sys.argv[5]
 except IndexError:
     print("Missing argument!")
     print(f"Usage: {sys.argv[0]} input-fifo output-fifo ctrl-fifo user_module")
     sys.exit(1)
 
-def with_time(text):
-    print(f'{datetime.now().strftime("%H:%M:%S")} - {text}')
 
-async def handle_control_stream(stream, context):
+class AppContext:
+    def __init__(self, log_stream):
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        log_handler = logging.StreamHandler(log_stream)
+        log_handler.setFormatter(formatter)
+        log_handler.setLevel(logging.DEBUG)
+
+        self.logger = logging.getLogger('scramjet-python-runner')
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(log_handler)
+
+
+async def handle_control_stream(context, stream):
     async for line in stream:
         try:
-            context['add'] = int(line)
-            with_time(f'Got ctrl message: {context}')
-        except ValueError:
-            print('Invalid control message - not a number:', line)
+            data = json.loads(line)
+            context.logger.warning(f'Got ctrl message: {data}')
+            if 'add' in data:
+                context.increment_result = data['add']
+        except json.JSONDecodeError:
+            context.logger.error(f'Invalid control message - not a valid JSON: {line}')
+
 
 async def main():
     async with aiofiles.open(INPUT, 'r') as f_in, \
                aiofiles.open(OUTPUT, 'w') as f_out, \
                aiofiles.open(CONTROL, 'r') as f_ctrl:
-        context = {}
-        asyncio.create_task(handle_control_stream(f_ctrl, context))
-        user_module = importlib.import_module(MODULE)
 
-        async def process(chunk):
-            return await transform(chunk, context)
+        # logging module doesn't work with aiofiles. However, writing to a
+        # named pipe should hopefully not incur a long wait.
+        with open(LOGGING, 'w') as f_log:
+            context = AppContext(f_log)
 
-        result = await user_module.transform(stream.iterate(f_in), context)
-        async with result.stream() as streamer:
-            async for chunk in streamer:
-                await f_out.write(str(chunk))
-                await f_out.flush()
+            context.logger.info('Opening control stream...')
+            asyncio.create_task(handle_control_stream(context, f_ctrl))
 
-        # asyncio.create_task(handle_control_stream(f_ctrl, context))
-        # await user_module.transform(context, f_in, f_out)
+            context.logger.info('Importing user module...')
+            user_module = importlib.import_module(MODULE)
+
+            result = await user_module.transform(context, stream.iterate(f_in))
+
+            async with result.stream() as streamer:
+                async for chunk in streamer:
+                    await f_out.write(str(chunk))
+                    await f_out.flush()
+
 
 asyncio.run(main())
