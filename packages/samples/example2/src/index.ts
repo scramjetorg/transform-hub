42a0b3c0d6bc0f845437a35ae73b080911b32dc1
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-08-03 14:36:06 +0000

    Refactor example2 to use in/out streams.

--- packages/samples/example2/src/index.ts
+++ packages/samples/example2/src/index.ts
@@ -1,13 +1,9 @@
-import { InertApp } from "@scramjet/types";
+import { TransformApp } from "@scramjet/types";
 import { DataStream } from "scramjet";
 
-
-const mod: InertApp = function(input, prefix: string = "Hello, ", suffix: string = "!") {
-    DataStream.from(process.stdin)
-        .map(name => prefix + name + suffix
-        )
-        .pipe(process.stdout);
-
+const mod: TransformApp = function(input, prefix: string = "Hello, ", suffix: string = "!") {
+    return DataStream.from(input)
+        .map(name => prefix + name + suffix);
 };
 
 export default mod;
