eb90c323197db95958d4b84cd68f3319bc9e3ce1
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-08-04 09:47:28 +0000

    Refactor

--- packages/samples/example2/src/index.ts
+++ packages/samples/example2/src/index.ts
@@ -1,9 +1,10 @@
 import { TransformApp } from "@scramjet/types";
 import { DataStream } from "scramjet";
 
-const mod: TransformApp = function(input, prefix: string = "Hello, ", suffix: string = "!") {
+
+const app: TransformApp = async function(input, prefix: string = "Hello, ", suffix: string = "!") {
     return DataStream.from(input)
         .map(name => prefix + name + suffix);
 };
 
-export default mod;
+export default app;
