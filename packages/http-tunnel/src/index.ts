9128f8aa48fee56073a27831accc7ac94b8797ed
patuwwy <patuwwy@gmail.com>
2021-08-12 15:40:58 +0000

    Establish communication tunnel to CPM

--- packages/http-tunnel/src/index.ts
+++ packages/http-tunnel/src/index.ts
@@ -1,25 +1,34 @@
-import * as http from "http";
-import { Agent, Server, ClientRequest } from "http";
+import { createServer, request, Agent, Server, ClientRequest } from "http";
 import { Socket } from "net";
 
 const BPMux = require("bpmux").BPMux;
-const bpmuxes: { [key: string]: any } = { };
+const defaultTunnelServerConfig = {
+    host: "0.0.0.0",
+    port: 11000
+};
+//const bpmuxes: { [key: string]: any } = {};
 
 export type GetMuxKey = {
     (req: ClientRequest): string;
 };
 
-export type ServerConfiguration = {
-    host: string,
-    port: number
+export type TunnelServerConfig = {
+    host?: string,
+    port?: number
+    head?: string
 };
 
-export function handleTunnelConnectRequest(apiServer: Server, server: Server, getMuxKey: GetMuxKey) {
+export function setServerTunnel(
+    sourceServer: Server,
+    tunnelServerConfig: TunnelServerConfig
+) {
+    tunnelServerConfig = { ...defaultTunnelServerConfig, ...tunnelServerConfig };
 
-    apiServer.on("request", (req, res) => {
-        console.log("CPM API: request", req.headers);
+    let mux: any;
 
-        const mux = bpmuxes[getMuxKey(req)];
+    sourceServer.on("request", (req, res) => {
+        console.log("CPM API: request");
+
         const muxedStream = mux.multiplex();
 
         muxedStream.pipe(res.socket);
@@ -27,40 +36,39 @@ export function handleTunnelConnectRequest(apiServer: Server, server: Server, ge
         req.socket.pipe(muxedStream);
     });
 
-    server.on("connect", (req, clientSocket, head) => {
-        console.log("CPM STH Server: STH Connected - Tunnel ready");
+    createServer()
+        .on("connect", (req, clientSocket, head) => {
+            console.log("CPM STH Server: STH Connected - Tunnel ready, head:", head.toString());
 
-        clientSocket.write("HTTP/1.1 200 \r\n\r\n");
+            clientSocket.write("HTTP/1.1 200 \r\n\r\n");
 
-        bpmuxes[getMuxKey(req)] = new BPMux(clientSocket);
-    });
-
+            mux = new BPMux(clientSocket);
+        })
+        .listen(tunnelServerConfig.port, tunnelServerConfig.host, () => {
+            console.log(`Tunnel Server on ${tunnelServerConfig.host}:${tunnelServerConfig.port}`);
+        });
 }
 
-export function sendConnectRequest(server: Server, serverConfig: ServerConfiguration) {
+export function setClientTunnel(targetServer: Server, tunnelServerConfig: TunnelServerConfig) {
     console.log("Establishing tunnel - Sending connect");
 
-    const agent = new Agent({ keepAlive: true });
-    const req = http.request({
-        port: serverConfig.port,
-        host: serverConfig.host,
-        method: "CONNECT",
-        agent
-    }, () => {
-        console.log("Connection handler");
-    });
+    tunnelServerConfig = { ...defaultTunnelServerConfig, ...tunnelServerConfig };
 
-    req.on("connect", (response, socket, head) => {
-        console.log("Tunnel: connect event");
+    const req = request({
+        port: tunnelServerConfig.port,
+        host: tunnelServerConfig.host,
+        method: "CONNECT",
+        agent: new Agent({ keepAlive: true })
+    }).on("connect", (_response, socket, head) => {
+        console.log("Tunnel established, head: ", head.toString());
 
         new BPMux(socket)
             .on("handshake", (mSocket: Socket) => {
                 console.log("handshake");
-                server.emit("connection", mSocket);
+                targetServer.emit("connection", mSocket);
             });
     });
 
+    req.write(tunnelServerConfig.head);
     req.end();
 }
-
-
