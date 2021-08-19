d9bd74e2a7563b30f13d8c710ee7cd27e073938b
patuwwy <patuwwy@gmail.com>
2021-08-19 16:28:11 +0000

    Update sending sequences list

--- packages/symbols/src/cpm-message-code.ts
+++ packages/symbols/src/cpm-message-code.ts
@@ -2,5 +2,10 @@ export enum CPMMessageCode {
     STH_ID = 7000,
     LOAD = 7001,
 
-    NETWORK_INFO = 7002
+    NETWORK_INFO = 7002,
+
+    INSTANCES = 7003,
+    SEQUENCES = 7004,
+    INSTANCE = 7005,
+    SEQUENCE = 7006
 }
