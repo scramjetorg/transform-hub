9c2e160d09b56866417c65298164468cceba8441
Alicja Gruzdz <alicja.gruzdz@gmail.com>
2021-08-24 08:39:59 +0000

    Add new message types

--- packages/symbols/src/index.ts
+++ packages/symbols/src/index.ts
@@ -3,3 +3,5 @@ export { CommunicationChannel } from "./communication-channel";
 export { RunnerMessageCode } from "./runner-message-code";
 export { SupervisorMessageCode } from "./supervisor-message-code";
 export { CPMMessageCode } from "./cpm-message-code";
+export { InstanceMessageCode } from "./instance-status-code";
+export { SequenceMessageCode } from "./sequence-status-code";
