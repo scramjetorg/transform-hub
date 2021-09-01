946f6f727c3f1a7c9f7f13fee1cd19769ece1909
Jan Warchoł <jan.warchol@scramjet.org>
2021-09-01 12:34:05 +0200

    Run two batches in test for synchronous draining
    We run the test twice because the results may differ for the first
    MAX_PARALLEL items (e.g. the algorithm may return placeholders etc.)

--- test-cases.py
+++ test-cases.py
@@ -89,6 +89,8 @@ objects_with_values = [
     in enumerate(TEST_SEQUENCE)
 ]
 MAX_PARALLEL = 4
+def monotonic_sequence(n):
+    return [{'id': i} for i in range(n)]
 
 async def test_write_then_read_concurrently(input_data):
     p = pyfca.Pyfca(MAX_PARALLEL, async_identity)
@@ -146,34 +148,59 @@ async def test_reads_after_end(input_data):
     log_results(results)
     assert results == input_data + [None]*4
 
-# TODO: pass one round of actual data - right now we're checking placeholders
-async def test_synchronous_writing(input_data):
-    loop = asyncio.get_event_loop()
-    event_loop_ran = False
-
-    def update_event_loop_status():
-        nonlocal event_loop_ran
-        event_loop_ran = True
-
-    def check_event_loop(expected):
-        log(f'Did event loop run already? {cyan}{event_loop_ran}{reset}')
-        assert event_loop_ran == expected
-
-    loop.call_soon(update_event_loop_status)
-
+# If the number of items being processed is below limit, write() should return
+# a future that resolves immediately (and therefore code that awaits it should
+# actually run synchronously).
+async def test_synchronous_draining(input_data):
     p = pyfca.Pyfca(MAX_PARALLEL, identity)
+    loop = asyncio.get_event_loop()
+    event_loop_flag = None
 
-    # Writes up till MAX_PARALLEL-th should resolve immediately
-    for x in input_data[:MAX_PARALLEL-1]:
-        drain = p.write(x)
-        log_drain_status(drain, x)
-        assert drain.done() == True
-        await drain
-        check_event_loop(False)
+    def start_sync_check():
+        log('The following sequence of instructions should be synchronous.')
+        nonlocal event_loop_flag
+        event_loop_flag = False
+        def update_flag():
+            log('Next event loop iteration.')
+            nonlocal event_loop_flag
+            event_loop_flag = True
+        # schedule for next event loop iteration
+        loop.call_soon(update_flag)
 
-    # pass control to event loop
-    await asyncio.sleep(0)
-    check_event_loop(True)
+    def check_async(expected):
+        log(f'Did next iteration of event loop start already? '
+            f'{cyan}{event_loop_flag}{reset}')
+        assert event_loop_flag == expected
+
+    async def write_and_await_drain(n):
+        for _ in range(n):
+            item = input_data.pop(0)
+            drain = p.write(item)
+            log_drain_status(drain, item)
+            assert drain.done() == True
+            await drain
+
+    # Note that we run the test twice because the results may differ for the
+    # first MAX_PARALLEL items (e.g. the algorithm may return placeholders etc.)
+    for i in range(2):
+        log(f'Start batch #{i+1}')
+        start_sync_check()
+
+        # Writes up till MAX_PARALLEL-1 should resolve immediately
+        await write_and_await_drain(MAX_PARALLEL - 1)
+        check_async(False)
+
+        # Create readers so the queue won't get stuck. This is still synchronous.
+        reads = [p.read() for _ in range(MAX_PARALLEL)]
+        check_async(False)
+
+        # MAX_PARALLEL-th write should reach the limit and awaiting on it
+        # should trigger entering event loop and processing previous items
+        await p.write(input_data.pop(0))
+        check_async(True)
+
+        # clean up the queueu.
+        await asyncio.gather(*reads)
 
 async def read_with_debug(pyfca, live_results=None):
     """Log received result and update result list immediately."""
@@ -292,7 +319,7 @@ tests_to_run = [
     (test_reads_before_write,                      objects_with_values),
     (test_reads_exceeding_writes,                  objects_with_values),
     (test_reads_after_end,                         objects_with_values),
-    (test_synchronous_writing,                     objects_with_values),
+    (test_synchronous_draining,                    monotonic_sequence(2*MAX_PARALLEL)),
     (test_limit_waiting_until_items_are_processed, objects_with_delays),
     (test_limit_waiting_for_reads,                 objects_with_values),
     (test_multitransform,                          objects_with_values),
