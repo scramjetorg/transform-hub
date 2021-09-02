c00f5ac5231c4c084ede4c7b05395ec11417132c
Alicja Gruzdz <alicja.gruzdz@gmail.com>
2021-09-02 20:32:51 +0000

    Fix ts err and modify path

--- scripts/dev/sd.ts
+++ scripts/dev/sd.ts
@@ -4,19 +4,20 @@ import * as fs from "fs";
 import { HostClient } from "@scramjet/api-client";
 import { StringDecoder } from "string_decoder";
 import { Stream } from "stream";
-const execPromise = (cmd: string, args: string[]) => new Promise<ChildProcess>((resolve, _reject) => {
+import { resolve } from "path";
+const execPromise = (cmd: string, args: string[]) => new Promise<ChildProcess>((res, _rej) => {
     const proc = spawn(cmd, args);
 
-    resolve(proc);
+    res(proc);
 });
-const waitForText = (stream: Stream, text: string) => new Promise<void>((resolve, _reject) => {
+const waitForText = (stream: Stream, text: string) => new Promise<void>((res, _rej) => {
     const decoder = new StringDecoder();
 
     stream.on("data", d => {
         const msg = decoder.write(d);
 
         if (msg.match(text)) {
-            resolve();
+            res();
         }
     });
 });
@@ -24,13 +25,18 @@ const waitForText = (stream: Stream, text: string) => new Promise<void>((resolve
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
     const host1 = "11111111-c201-4fe4-8309-111111111111";
-    const sequence1 = fs.createReadStream("../../../sth/packages/reference-apps/endless-names-output.tar.gz");
-    const sequence2 = fs.createReadStream("../../../sth/packages/reference-apps/hello-input-out.tar.gz");
+    const sequence1 = fs.createReadStream(resolve(__dirname, "../../../sth/packages/reference-apps/endless-names-output.tar.gz"));
+    const sequence2 = fs.createReadStream(resolve(__dirname, "../../../sth/packages/reference-apps/hello-input-out.tar.gz"));
 
     // host 1
     fs.writeFileSync("/tmp/sth-id.json", JSON.stringify({ id: host1 }));
 
-    const host1Process = await execPromise("ts-node", ["../../../sth/packages/sth/src/bin/hub"]);
+    const host1Process = await execPromise("ts-node", [resolve(__dirname, "../../../sth/packages/sth/src/bin/hub")]);
+
+    if (!host1Process.stdout || !host1Process.stderr) {
+        console.error("Spawned STH process stdio is undefined.");
+        process.exit(1);
+    }
 
     host1Process.stdout.pipe(process.stdout);
     host1Process.stderr.pipe(process.stderr);
@@ -52,5 +58,6 @@ const waitForText = (stream: Stream, text: string) => new Promise<void>((resolve
 
     //await inst2.sendStream("input", inst1Output, { type: "application/x-ndjson", end: false });
 
-    (await inst2.getStream("output")).data.pipe(process.stdout);
+    (await inst2.getStream("output")).data?.pipe(process.stdout);
 })();
+
