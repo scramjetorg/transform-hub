642f34f41e5c15286f23625db8e583e0c5063b96
patuwwy <patuwwy@gmail.com>
2021-09-14 07:34:06 +0000

    Update lint, update sd script

--- scripts/dev/sd.ts
+++ scripts/dev/sd.ts
@@ -4,23 +4,11 @@ import * as fs from "fs";
 import { StringDecoder } from "string_decoder";
 import { Stream } from "stream";
 import { resolve } from "path";
-import { CPMClient } from "@scramjet/cpm-api-client";
 import { HostClient } from "@scramjet/api-client";
 
 const scenarios = `
 /**
  * 1 - Send data from one instance to another one on the same host and get it's output.
- * 2 - Send data to CPM, start instance consuming data, get it's output.
- * 3 - Send data to STH1, get data from CPM
- * 4 - Send data to STH1, get data from STH2 (opr)
- *
- * 5 - Send to STH, received via CPM api
- * 6 - Send to STH1 get from STH1
- * 7 - Send to STH1 get output instance on STH2
- * 8 - Instance on STH1 provides data, Get from CPM
- * 9 - Instance on STH1 provided data, get from STH2
- * 10 - send to CPM, get from STH1
- *
  */
 `;
 
@@ -58,20 +46,9 @@ const waitForText = (stream: Stream, text: string) => new Promise<void>((res, _r
     });
 
     const host1 = "11111111-c201-4fe4-8309-111111111111";
-    const host2 = "22222222-c201-4fe4-8309-222222222222";
     const sequence1 = fs.createReadStream(resolve(__dirname, "../../../sth/packages/reference-apps/endless-names-output.tar.gz"));
     const sequence2 = fs.createReadStream(resolve(__dirname, "../../../sth/packages/reference-apps/hello-input-out.tar.gz"));
     //
-    const startCMP = async () => {
-        const cpmProcess = await execPromise("ts-node", ["../../../cpm/packages/cpm/src/bin/cpm"]);
-
-        await waitForText(cpmProcess.stdout, "STH API");
-
-        cpmProcess.stdout.pipe(process.stdout);
-        cpmProcess.stderr.pipe(process.stderr);
-
-        console.log("CPM started");
-    };
     //
     const startHost1 = async (config: { cpm: boolean }) => {
         fs.writeFileSync("/tmp/sth-id.json", JSON.stringify({ id: host1 }));
@@ -99,35 +76,6 @@ const waitForText = (stream: Stream, text: string) => new Promise<void>((res, _r
 
         console.log("Host 1 started");
     };
-    const startHost2 = async (config: { cpm: boolean }) => {
-        fs.writeFileSync("/tmp/sth-id.json", JSON.stringify({ id: host2 }));
-
-        let args = [resolve(__dirname, "../../../sth/packages/sth/src/bin/hub"), "-S", "/tmp/s2", "-P", "8002"];
-
-        if (config.cpm) {
-            args = args.concat(["-C", "localhost:7000"]);
-        }
-
-        const host2Process = await execPromise("ts-node", args);
-
-        host2Process.stdout.pipe(process.stdout);
-        host2Process.stderr.pipe(process.stderr);
-
-        if (config.cpm) {
-            await waitForText(host2Process.stdout, "Received id");
-        } else {
-            await waitForText(host2Process.stdout, "API listening");
-        }
-
-        console.log("Host 2 started");
-    };
-    const cpmClient = new CPMClient("http://localhost:9000/api/v1");
-
-    /*
-    await startCMP();
-    await startHost1();
-    await startHost2();
-    */
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let seq1, seq2, inst1, inst2;
@@ -146,69 +94,6 @@ const waitForText = (stream: Stream, text: string) => new Promise<void>((res, _r
         inst2 = await seq2.start({}, []);
 
         (await inst2.getStream("output")).data?.pipe(process.stdout);
-
-        break;
-    case 2:
-        //Send data to CPM, start instance consuming data, get it's output.
-        await startCMP();
-        await startHost1({ cpm: true });
-
-        seq2 = await cpmClient.getHostClient(host1).sendSequence(sequence2);
-
-        await cpmClient.sendNamedData(
-            "names",
-            fs.createReadStream(resolve(__dirname, "names.json")),
-            "application/x-ndjson"
-        ).then(() => {
-            console.log("resolved");
-        }).catch(e => {
-            console.error(e);
-        });
-
-        inst2 = await seq2.start({}, []);
-
-        (await inst2.getStream("output")).data?.pipe(process.stdout);
-
-        break;
-    case 3:
-        await startCMP();
-        await startHost1({ cpm: true });
-
-        const hostClient = new HostClient("http://localhost:8001/api/v1");
-
-        await hostClient.sendNamedData(
-            "names",
-            fs.createReadStream(resolve(__dirname, "names.json")),
-            "application/x-ndjson"
-        ).then(() => {
-            console.log("resolved");
-        }).catch(e => {
-            console.error(e);
-        });
-
-        (await cpmClient.getNamedData("names")).data?.pipe(process.stdout);
-
-        break;
-    case 4:
-        //Send data to STH1, get data from STH2 (opr)
-        await startCMP();
-        await startHost1({ cpm: true });
-        await startHost2({ cpm: true });
-
-        const hostClient1 = new HostClient("http://localhost:8001/api/v1");
-        const hostClient2 = new HostClient("http://localhost:8002/api/v1");
-
-        await hostClient1.sendNamedData(
-            "names",
-            fs.createReadStream(resolve(__dirname, "names.json")),
-            "application/x-ndjson"
-        ).then(() => {
-            console.log("resolved");
-        }).catch(e => {
-            console.error(e);
-        });
-
-        (await hostClient2.getNamedData("names")).data?.pipe(process.stdout);
 
         break;
     default:
