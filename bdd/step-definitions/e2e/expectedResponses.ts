861f2bc305f0833f2f9b6d92ecf77c69850c6fe3
patuwwy <patuwwy@gmail.com>
2021-09-22 11:27:02 +0000

    BDD instance to instance

--- bdd/step-definitions/e2e/expectedResponses.ts
+++ bdd/step-definitions/e2e/expectedResponses.ts
@@ -1,6 +1,15 @@
 /* eslint-disable quotes */
 export const expectedResponses: { [key:string]: any} = {
-    "hello-input-out-10": `{ "name": "Aga" }\n{ "name": "Michał" }\n{ "name": "Patryk" }\n{ "name": "Rafał" }\n{ "name": "Aida" }\n{ "name": "Basia" }\n{ "name": "Natalia" }\n{ "name": "Monika" }\n{ "name": "Wojtek" }\n`,
+    "endless-names-10": `{ "name": "Aga" }\n{ "name": "Michał" }\n{ "name": "Patryk" }\n{ "name": "Rafał" }\n{ "name": "Aida" }\n{ "name": "Basia" }\n{ "name": "Natalia" }\n{ "name": "Monika" }\n{ "name": "Wojtek" }\n`,
     "nyc-city": `{ \"city\": \"New York\" }`,
-    hulkName: "Name is: Hulk\n"
+    hulkName: "Name is: Hulk\n",
+    "hello-input-out-10": 'Name is: Aga\n' +
+       'Name is: Michał\n' +
+       'Name is: Patryk\n' +
+       'Name is: Rafał\n' +
+       'Name is: Aida\n' +
+       'Name is: Basia\n' +
+       'Name is: Natalia\n' +
+       'Name is: Monika\n' +
+       'Name is: Wojtek\n'
 };
