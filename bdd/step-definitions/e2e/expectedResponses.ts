6f3c3be8b9e5184583716ba50f539f91411901a1
a-tylenda <agnieszka.tylenda@scramjet.com>
2021-10-04 15:25:48 +0000

    Fixing service discovery CLI

--- bdd/step-definitions/e2e/expectedResponses.ts
+++ bdd/step-definitions/e2e/expectedResponses.ts
@@ -1,8 +1,18 @@
 /* eslint-disable quotes */
 export const expectedResponses: { [key:string]: any} = {
     "endless-names-10": `{"name":"Alice"}\n{"name":"Ada"}\n{"name":"Aga"}\n{"name":"Michał"}\n{"name":"Patryk"}\n{"name":"Rafał"}\n{"name":"Aida"}\n{"name":"Basia"}\n{"name":"Natalia"}\n{"name":"Monika"}\n{"name":"Wojtek"}\n`,
+    "nyc-city-nl": `{ \"city\": \"New York\" }\n`,
     "nyc-city": `{ \"city\": \"New York\" }`,
-    hulkName: "Name is: Hulk\n",
+    "hulkName": "Name is: Hulk\n",
+    "hello-avengers":
+        'Name is: Ant-Man\n' +
+        'Name is: Iron Man\n' +
+        'Name is: Hulk\n' +
+        'Name is: Hawkeye\n' +
+        'Name is: Black Widow\n' +
+        'Name is: Thor\n' +
+        'Name is: Captain America\n' +
+        'Name is: Spider-Man\n',
     "hello-input-out-10":
         'Name is: Alice\n' +
         'Name is: Ada\n' +
