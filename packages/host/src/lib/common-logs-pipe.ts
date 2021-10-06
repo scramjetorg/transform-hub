1b1498a5cfb787a799bd986eb0121a08bc6a95e0
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-10-06 13:36:25 +0000

    Run colorizeId only once per instance.

--- packages/host/src/lib/common-logs-pipe.ts
+++ packages/host/src/lib/common-logs-pipe.ts
@@ -15,7 +15,7 @@ const prefixWithInstanceId = (instanceId: string) => new Transform({
         const chunkStr: string = chunk.toString("utf-8");
 
         chunkStr.trimEnd().split("\n").forEach((line: string) => {
-            this.push(colorizeId(instanceId) + ": " + line + "\n");
+            this.push(instanceId + ": " + line + "\n");
         });
         done();
     }
@@ -26,7 +26,7 @@ export class CommonLogsPipe {
 
     public addInStream(instanceId: string, stream: Readable): void {
         stream
-            .pipe(prefixWithInstanceId(instanceId))
+            .pipe(prefixWithInstanceId(colorizeId(instanceId)))
             .pipe(this.outStream, { end: false });
     }
 
