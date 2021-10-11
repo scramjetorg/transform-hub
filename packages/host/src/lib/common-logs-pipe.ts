6bd666e00648928f77e8e1c8092519cc64eda8b1
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-10-11 10:12:57 +0000

    Remove colorization.

--- packages/host/src/lib/common-logs-pipe.ts
+++ packages/host/src/lib/common-logs-pipe.ts
@@ -1,16 +1,6 @@
 import { ReReadable } from "rereadable-stream";
 import { Readable, Transform } from "stream";
 
-const colors = ["[31m", "[32m", "[33m", "[34m", "[35m", "[36m", "[37m"];
-
-const withColor = (txt: string, color: string) => `\x1b${color}${txt}\x1b[0m`;
-
-const colorizeId = (instanceId: string): string => {
-    const index = parseInt(instanceId.split("-")[0], 16) % colors.length;
-
-    return withColor(instanceId, colors[index]);
-};
-
 const prefixWithInstanceId = (instanceId: string) => new Transform({
     transform(chunk, encoding, done) {
         const chunkStr: string = chunk.toString("utf-8");
@@ -33,7 +23,7 @@ export class CommonLogsPipe {
 
     public addInStream(instanceId: string, stream: Readable): void {
         stream
-            .pipe(prefixWithInstanceId(colorizeId(instanceId)))
+            .pipe(prefixWithInstanceId(instanceId))
             .pipe(this.outStream, { end: false });
     }
 
