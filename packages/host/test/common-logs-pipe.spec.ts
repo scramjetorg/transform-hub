e060e9c3e82c1e4b9bdf61946eb3ca2819c38ac7
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-10-12 09:28:37 +0000

    Refactor tests.

--- packages/host/test/common-logs-pipe.spec.ts
+++ packages/host/test/common-logs-pipe.spec.ts
@@ -6,38 +6,11 @@ import test from "ava";
 
 const lineLength = 20;
 const numberOfLogs = 1e4;
-const highWaterMark = lineLength * numberOfLogs / 4;
-const commonLogsBufferLength = numberOfLogs / 4;
+const highWaterMark = lineLength * numberOfLogs / 1000;
+const commonLogsBufferLength = numberOfLogs / 1000;
 
 test("10k logs pauses instances streams if commonLogsPipe is a PassThrough", async (t) => {
     const commonLogsPipe = { outStream: new PassThrough({ highWaterMark }) };
-
-    const instances = [new PassThrough(), new PassThrough()];
-
-    instances.forEach((instance, _index) => {
-        instance.on("data", (data) => /* consume data */ data);
-        instance.pipe(commonLogsPipe.outStream);
-    });
-
-    await DataStream.from(async function* () {
-        let i = 0;
-
-        while (i < numberOfLogs) {
-            yield ++i;
-        }
-    })
-        .do((index) => {
-            instances.forEach(instance => {
-                instance.write(`Log ${index}`);
-            });
-        })
-        .run();
-
-    t.assert(instances.every(instance => instance.isPaused() === true));
-});
-
-test("10k logs pauses instances streams if commonLogsPipe is a PassThrough", async (t) => {
-    const commonLogsPipe = { outStream: new PassThrough({ highWaterMark: 10, objectMode: true }) };
 
     const instances = [new PassThrough(), new PassThrough()];
 
