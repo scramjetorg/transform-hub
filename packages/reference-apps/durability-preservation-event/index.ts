a8333947876a18e654d737df4e805d6957c97da8
patuwwy <patuwwy@gmail.com>
2021-10-24 21:47:22 +0000

    Add sequence downloading file in interval and emiting event

---
+++ packages/reference-apps/durability-preservation-event/index.ts
@@ -0,0 +1,39 @@
+import * as https from "https";
+
+import { AppContext } from "@scramjet/types";
+import { IncomingMessage } from "http";
+
+const downloadFile = (url: string): Promise<number | undefined> => {
+    return new Promise((resolve) => {
+        https.get(url, (response: IncomingMessage) => {
+            resolve(response.statusCode);
+        });
+    });
+};
+
+/**
+ * @param this - this
+ * @param _inputStream input stream
+ * @param file url to download
+ * @param interval time interval
+ * @returns { Promise<void> } Promise
+ *
+ */
+export = async function(this: AppContext<any, any>, _inputStream: any, file: string, interval: number) {
+    return new Promise(resolve => {
+        this.addStopHandler(resolve);
+
+        setInterval(async () => {
+            const statusCode = await downloadFile(file);
+
+            const status = {
+                statusCode,
+                date: new Date().toISOString()
+            };
+
+            this.emit("status", status);
+            this.logger.log(status);
+        }, interval * 1000);
+    });
+}
+
