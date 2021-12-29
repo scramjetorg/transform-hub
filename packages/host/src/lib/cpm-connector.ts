7dc6be5c46ae93d81fae5ee733d4436a363f06c0
patuwwy <patuwwy@gmail.com>
2021-12-29 16:37:35 +0000

    Change passing Verser url to VerserClient

--- packages/host/src/lib/cpm-connector.ts
+++ packages/host/src/lib/cpm-connector.ts
@@ -51,14 +51,11 @@ export class CPMConnector extends EventEmitter {
 
     constructor(cpmUrl: string, config: CPMConnectorOptions, server: Server) {
         super();
+        this.cpmURL = cpmUrl;
         this.config = config;
-        this.cpmURL = cpmUrl.indexOf("//") == -1 ? `http://${this.cpmURL}` : cpmURL;
-
-        const cpmAddress = this.cpmURL;
 
         this.verserClient = new VerserClient({
-            remotePort: +cpmAddress.port,
-            remoteHost: cpmAddress.hostname,
+            verserUrl: `http://${cpmUrl}/verser`,
             headers: {},
             server
         });
