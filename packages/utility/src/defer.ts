881ce47a1c17b7afcccb2b464e695e951712ddbd
patuwwy <patuwwy@gmail.com>
2022-01-03 11:20:59 +0000

    Comments in @scramjet/utility

--- packages/utility/src/defer.ts
+++ packages/utility/src/defer.ts
@@ -1,2 +1,11 @@
+/**
+ * Returns a promise that resolves after the specified duration.
+ * @example
+ * // waits for 10 second
+ * await defer(10 * 1000);
+ *
+ * @param {number} timeout - timeout in milliseconds
+ * @returns {Promise<void>} - promise that resolves after timeout
+ */
 export const defer = (timeout: number): Promise<void> =>
     new Promise(res => setTimeout(res, timeout));
