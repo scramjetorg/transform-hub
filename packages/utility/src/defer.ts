88ac941fccd2fb7b3bfec43aa695cbc076b7229a
Patryk Falba <patuwwy@gmail.com>
2022-01-03 12:55:06 +0100

    Apply suggestions from code review

--- packages/utility/src/defer.ts
+++ packages/utility/src/defer.ts
@@ -4,8 +4,8 @@
  * // waits for 10 second
  * await defer(10 * 1000);
  *
- * @param {number} timeout - timeout in milliseconds
- * @returns {Promise<void>} - promise that resolves after timeout
+ * @param {number} timeout - timeout in milliseconds.
+ * @returns {Promise<void>} - promise that resolves after timeout.
  */
 export const defer = (timeout: number): Promise<void> =>
     new Promise(res => setTimeout(res, timeout));
