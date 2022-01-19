6513e35208d7de227becc2d5d1152ab8af564a7e
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2022-01-19 10:13:26 +0000

    Fix imports.

--- packages/host/src/lib/host.ts
+++ packages/host/src/lib/host.ts
@@ -12,12 +12,11 @@ import { CPMConnector } from "./cpm-connector";
 import { CSIController } from "./csi-controller";
 import { CommonLogsPipe } from "./common-logs-pipe";
 import { InstanceStore } from "./instance-store";
-import { DockerodeDockerHelper, getSequenceAdapter } from "@scramjet/adapters";
+import { DockerodeDockerHelper, getSequenceAdapter, setupDockerNetworking } from "@scramjet/adapters";
 import { ReasonPhrases } from "http-status-codes";
 import { ServiceDiscovery } from "./sd-adapter";
 import { SocketServer } from "./socket-server";
 import { LoadCheck } from "@scramjet/load-check";
-import { setupDockerNetworking } from "@scramjet/adapters/src/docker-networking";
 
 const version = findPackage(__dirname).next().value?.version || "unknown";
 
