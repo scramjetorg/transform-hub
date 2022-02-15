c060bf2c01861a7b5015409733e2aca1e11c1ef5
Eryk Solecki <eryk.solecki@scramjet.org>
2022-02-15 12:37:21 +0000

    csv-transform sequence

---
+++ packages/reference-apps/csv-transform/index.ts
@@ -0,0 +1,58 @@
+import { TransformApp } from "@scramjet/types";
+import { PassThrough } from "stream";
+import { createHash } from "crypto";
+import { createInterface } from "readline";
+import { get } from "https";
+
+const getFirstLineFromUrlFilePromise = async (url: string) => {
+    return new Promise<string>((res, rej) => {
+        get(url, (response) => {
+            const rl = createInterface({ input: response });
+
+            rl.on("line", (line) => {
+                response.destroy();
+                res(line);
+            });
+        }).on("error", rej);
+    });
+};
+
+// Simple seqence receiving line of text and passing to the output:
+// received line + md5 of line + first line of file from storeItemUrl
+// seqeuence destroys outputStream if it's unable to get line from storeItemUrl
+const app: TransformApp<string> = async function(input: any) {
+    const outputStream = new PassThrough({ encoding: "utf-8" });
+    const rl = createInterface({ input });
+    let drain: null | Promise<void> = null;
+
+    const storeItemUrl = "https://assets.scramjet.org/scp-store/helloworld.txt";
+    let storeFirstLine: string = "";
+
+    rl.on("line", async (line) => {
+        const out = outputStream.write(
+            line + ";" + createHash("md5").update(line).digest("hex") + ";" + storeFirstLine + "\n"
+        );
+
+        if (!out) {
+            input.pause();
+            drain = new Promise<void>((res, rej) => outputStream.on("drain", res).on("error", rej).on("close", rej));
+            await drain;
+            input.resume();
+        }
+    });
+    rl.on("close", async () => {
+        await drain;
+        outputStream.end();
+    });
+
+    try {
+        storeFirstLine = await getFirstLineFromUrlFilePromise(storeItemUrl);
+    } catch (error: any) {
+        outputStream.destroy(error);
+        input.end();
+    }
+
+    return outputStream;
+};
+
+export default app;
