d19af5dc58cdd9de6e68d8afb0fe4cfb524d308f
Michal Piotrowski <michal.piotrowski@scramjet.org>
2022-02-18 16:31:39 +0100

    Change formatting in Python Runner

--- python/runner/runner.py
+++ python/runner/runner.py
@@ -19,7 +19,7 @@ instance_id = os.getenv('INSTANCE_ID')
 
 def send_encoded_msg(stream, msg_code, data={}):
     message = json.dumps([msg_code.value, data])
-    stream.write(f"{message}\r\n".encode())
+    stream.write(f'{message}\r\n'.encode())
 
 
 class Runner:
@@ -34,7 +34,7 @@ class Runner:
 
 
     async def main(self, server_host, server_port):
-        self.logger.info("Connecting to host...")
+        self.logger.info('Connecting to host...')
         await self.init_connections(server_host, server_port)
 
         # Do this early to have access to any thrown exceptions and logs.
@@ -42,7 +42,7 @@ class Runner:
         self.connect_log_stream()
 
         config, args = await self.handshake()
-        self.logger.info("Communication established.")
+        self.logger.info('Communication established.')
         asyncio.create_task(self.connect_control_stream())
         asyncio.create_task(self.setup_heartbeat())
 
@@ -52,14 +52,14 @@ class Runner:
 
     async def init_connections(self, host, port):
         async def connect(channel):
-            self.logger.debug(f"Connecting to {host}:{port}...")
+            self.logger.debug(f'Connecting to {host}:{port}...')
             reader, writer = await asyncio.open_connection(host, port)
-            self.logger.debug("Connected.")
+            self.logger.debug('Connected.')
 
             writer.write(self.instance_id.encode())
             writer.write(channel.value.encode())
             await writer.drain()
-            self.logger.debug(f"Sent ID {self.instance_id} on {channel.name}.")
+            self.logger.debug(f'Sent ID {self.instance_id} on {channel.name}.')
 
             return (channel, reader, writer)
 
@@ -77,35 +77,35 @@ class Runner:
 
 
     def connect_stdio(self):
-        sys.stdout = codecs.getwriter("utf-8")(self.streams[CC.STDOUT])
-        sys.stderr = codecs.getwriter("utf-8")(self.streams[CC.STDERR])
-        sys.stdin = Stream.read_from(self.streams[CC.STDIN]).decode("utf-8")
+        sys.stdout = codecs.getwriter('utf-8')(self.streams[CC.STDOUT])
+        sys.stderr = codecs.getwriter('utf-8')(self.streams[CC.STDERR])
+        sys.stdin = Stream.read_from(self.streams[CC.STDIN]).decode('utf-8')
         # pretend to have API compatibiliy
         sys.stdout.flush = lambda: True
         sys.stderr.flush = lambda: True
-        self.logger.info("Stdio connected.")
+        self.logger.info('Stdio connected.')
 
 
     def connect_log_stream(self):
-        self.logger.info("Switching to main log stream...")
-        target = codecs.getwriter("utf-8")(self.streams[CC.LOG])
+        self.logger.info('Switching to main log stream...')
+        target = codecs.getwriter('utf-8')(self.streams[CC.LOG])
         self._logging_setup.switch_to(target)
         self._logging_setup.flush_temp_handler()
-        self.logger.info("Log stream connected.")
+        self.logger.info('Log stream connected.')
 
 
     async def handshake(self):
         monitoring = self.streams[CC.MONITORING]
         control = self.streams[CC.CONTROL]
 
-        self.logger.info(f"Sending PING")
+        self.logger.info(f'Sending PING')
         send_encoded_msg(monitoring, msg_codes.PING)
 
-        message = await control.readuntil(b"\n")
-        self.logger.info(f"Got message: {message}")
+        message = await control.readuntil(b'\n')
+        self.logger.info(f'Got message: {message}')
         code, data = json.loads(message.decode())
 
-        self.logger.info(f"Sending PANG")
+        self.logger.info(f'Sending PANG')
         pang_requires_data = {
             'requires': '',
             'contentType': ''
@@ -113,7 +113,7 @@ class Runner:
         send_encoded_msg(monitoring, msg_codes.PANG, pang_requires_data)
 
         if code == msg_codes.PONG.value:
-            self.logger.info(f"Got configuration: {data}")
+            self.logger.info(f'Got configuration: {data}')
             return data['appConfig'], data['args']
 
 
@@ -126,7 +126,7 @@ class Runner:
                 .decode('utf-8').split('\n').map(json.loads)
         )
         async for code, data in control_messages:
-            self.logger.debug(f"Control message received: {code} {data}")
+            self.logger.debug(f'Control message received: {code} {data}')
             if code == msg_codes.KILL.value:
                 self.exit_immediately()
             if code == msg_codes.STOP.value:
@@ -141,14 +141,14 @@ class Runner:
             await stop
         # TODO: add canCallKeppAlive feature, asyncio.wait?
         # canCallKeepAlive = data.get('canCallKeppalive')
-        self.logger.info(f"Gracefully shutting down...{data}")
+        self.logger.info(f'Gracefully shutting down...{data}')
         try:
             timeout = data.get('timeout') / 1000
             await asyncio.sleep(timeout)
             send_encoded_msg(self.streams[CC.MONITORING], msg_codes.SEQUENCE_STOPPED, {})
             self.exit_immediately()
         except Exception as e:
-            self.logger.error("Error stopping sequence", e)
+            self.logger.error('Error stopping sequence', e)
             send_encoded_msg(self.streams[CC.MONITORING], msg_codes.SEQUENCE_STOPPED, e)
 
 
@@ -167,11 +167,11 @@ class Runner:
         module_dir = os.path.dirname(self.seq_path)
         if module_dir not in sys.path:
             sys.path.append(module_dir)
-        self.logger.debug(f"Loading sequence from {self.seq_path}...")
-        spec = importlib.util.spec_from_file_location("sequence", self.seq_path)
+        self.logger.debug(f'Loading sequence from {self.seq_path}...')
+        spec = importlib.util.spec_from_file_location('sequence', self.seq_path)
         self.sequence = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(self.sequence)
-        self.logger.info(f"Sequence loaded: {self.sequence}")
+        self.logger.info(f'Sequence loaded: {self.sequence}')
         # switch to sequence dir so that relative paths will work
         os.chdir(os.path.dirname(self.seq_path))
 
@@ -181,10 +181,10 @@ class Runner:
         input_stream = Stream()
         asyncio.create_task(self.connect_input_stream(input_stream))
 
-        self.logger.info("Running instance...")
+        self.logger.info('Running instance...')
         result = self.sequence.run(context, input_stream, *args)
 
-        self.logger.info(f"Sending PANG")
+        self.logger.info(f'Sending PANG')
         monitoring = self.streams[CC.MONITORING]
         pang_provides_data = {
             'provides': '',
@@ -197,47 +197,47 @@ class Runner:
         if result:
             await self.forward_output_stream(result)
         else:
-            self.logger.debug("Sequence returned no output.")
+            self.logger.debug('Sequence returned no output.')
 
         self.logger.info('Finished.')
 
 
     async def connect_input_stream(self, input_stream):
-        raw_headers = await self.streams[CC.IN].readuntil(b"\r\n\r\n")
-        header_list = raw_headers.decode().rstrip().split("\r\n")
+        raw_headers = await self.streams[CC.IN].readuntil(b'\r\n\r\n')
+        header_list = raw_headers.decode().rstrip().split('\r\n')
         headers = {
-            key.lower(): val for key, val in [el.split(": ") for el in header_list]
+            key.lower(): val for key, val in [el.split(': ') for el in header_list]
         }
-        self.logger.info(f"Input headers: {repr(headers)}")
+        self.logger.info(f'Input headers: {repr(headers)}')
 
         input_type = headers['content-type']
-        if input_type == "text/plain":
+        if input_type == 'text/plain':
             input = Stream.read_from(self.streams[CC.IN])
-            self.logger.debug("Decoding input stream...")
-            input = input.decode("utf-8")
-        elif input_type == "application/octet-stream":
-            self.logger.debug("Opening input in binary mode...")
+            self.logger.debug('Decoding input stream...')
+            input = input.decode('utf-8')
+        elif input_type == 'application/octet-stream':
+            self.logger.debug('Opening input in binary mode...')
             input = Stream.read_from(self.streams[CC.IN], chunk_size=CHUNK_SIZE)
         else:
-            raise TypeError(f"Unsupported input type: {repr(input_type)}")
+            raise TypeError(f'Unsupported input type: {repr(input_type)}')
 
         input.pipe(input_stream)
-        self.logger.debug("Input stream forwarded to the instance.")
+        self.logger.debug('Input stream forwarded to the instance.')
 
 
     async def forward_output_stream(self, output):
         try:
             output_type = self.sequence.output_type
         except AttributeError:
-            self.logger.debug("Output type not set, using default")
-            output_type = "text/plain"
-        self.logger.info(f"Output type: {output_type}")
+            self.logger.debug('Output type not set, using default')
+            output_type = 'text/plain'
+        self.logger.info(f'Output type: {output_type}')
 
-        if output_type == "text/plain":
-            self.logger.debug("Output stream will be treated as text and encoded")
+        if output_type == 'text/plain':
+            self.logger.debug('Output stream will be treated as text and encoded')
             output = output.map(lambda s: s.encode())
-        if output_type == "application/x-ndjson":
-            self.logger.debug("Output will be converted to JSON")
+        if output_type == 'application/x-ndjson':
+            self.logger.debug('Output will be converted to JSON')
             output = output.map(lambda chunk: (json.dumps(chunk)+'\n').encode())
 
         await output.write_to(self.streams[CC.OUT])
@@ -264,7 +264,7 @@ class AppContext:
     def on(self, event_name, callback):
         self.emitter.on(event_name, callback)
 
-    def emit(self, event_name, message=""):
+    def emit(self, event_name, message=''):
         send_encoded_msg(
             self.monitoring,
             msg_codes.EVENT,
@@ -274,13 +274,13 @@ class AppContext:
 
 log_setup = LoggingSetup(STARTUP_LOGFILE)
 
-log_setup.logger.info("Starting up...")
-log_setup.logger.debug(f"server_port: {server_port}")
-log_setup.logger.debug(f"instance_id: {instance_id}")
-log_setup.logger.debug(f"sequence_path: {sequence_path}")
+log_setup.logger.info('Starting up...')
+log_setup.logger.debug(f'server_port: {server_port}')
+log_setup.logger.debug(f'instance_id: {instance_id}')
+log_setup.logger.debug(f'sequence_path: {sequence_path}')
 
 if not sequence_path or not server_port or not instance_id:
-    log_setup.logger.error("Undefined config variable! <blows raspberry>")
+    log_setup.logger.error('Undefined config variable! <blows raspberry>')
     sys.exit(2)
 
 runner = Runner(instance_id, sequence_path, log_setup)
