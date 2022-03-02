03f5c45e9e6281c06bcc99bb2f76ca5c3c58b298
Michal Piotrowski <michal.piotrowski@scramjet.org>
2022-03-02 12:19:48 +0100

    Fixes for Dockerfile context

--- packages/adapters/src/process-instance-adapter.ts
+++ packages/adapters/src/process-instance-adapter.ts
@@ -57,9 +57,7 @@ IComponent {
 
         if ("python3" in config.engines) {
             this.logger.trace(gotPython);
-            const runnerPath = isTSNode
-                ? "../../python-runner/runner.py"
-                : path.resolve(__dirname, require.resolve("@scramjet/python-runner"));
+            const runnerPath = path.resolve(__dirname, require.resolve("@scramjet/python-runner"));
 
             return [
                 "/usr/bin/env",
@@ -81,9 +79,7 @@ IComponent {
     getPythonpath() {
         let pythonpath = path.resolve(
             __dirname,
-            isTSNode
-                ? "../../python-runner/dist"
-                : path.resolve(__dirname, require.resolve("@scramjet/python-runner"), "..")
+            path.resolve(__dirname, require.resolve("@scramjet/python-runner"), "..")
         );
 
         if (process.env.PYTHONPATH) pythonpath += `:${process.env.PYTHONPATH}`;
