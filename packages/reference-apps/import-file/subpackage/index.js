444bbe1860e35e623c366a81b798a59dc575549b
Budleigh Salterton <cz@scramjet.org>
2022-03-04 10:42:49 +0000

    Fix packages, remove one that doesn't work in node

--- packages/reference-apps/import-file/subpackage/index.js
+++ packages/reference-apps/import-file/subpackage/index.js
@@ -1,4 +1,8 @@
-/* eslint-disable no-loop-func */
+/* eslint-disable no-console */
+
+async function defer() {
+    return new Promise(res => setTimeout(res, 1000));
+}
 
 /**
  * An inert app that is just a function (not wrapped)
@@ -6,9 +10,10 @@
  * @type {InertApp}
  * @param _stream - dummy input stream
  */
- module.exports = async function(_stream) {
+module.exports = async function(_stream) {
     while (++x < 5) {
         console.log({ x: x });
-        await new Promise(res => setTimeout(res, 1000));
+        await defer();
     }
 };
+
