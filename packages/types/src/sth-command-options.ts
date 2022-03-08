da5ff8141f02639c0f6df18cb991cab0c68981b1
patuwwy <patuwwy@gmail.com>
2022-03-08 23:13:14 +0000

    Update passing command options to STH

---
+++ packages/types/src/sth-command-options.ts
@@ -0,0 +1,24 @@
+import { LogLevel } from "./object-logger";
+
+export type STHCommandOptions = {
+    logLevel: LogLevel;
+    port: number;
+    hostname: string;
+    identifyExisting: boolean;
+    cpmUrl?: string;
+    id?: string;
+    runtimeAdapter: string;
+    runnerImage: string;
+    runnerMaxMem: number;
+    prerunnerImage: string;
+    prerunnerMaxMem: number;
+    exposeHostIp: string;
+    instancesServerPort: string;
+    sequencesRoot: string;
+    k8sNamespace: string;
+    k8sAuthConfigPath: string;
+    k8sSthPodHost: string;
+    k8sRunnerImage: string;
+    k8sSequencesRoot: string;
+    docker: boolean;
+}
