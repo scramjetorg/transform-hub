ba481086759ff4f4b9d64737240f6c8eb7c1b1e8
Tomasz Kociołek <tomasz.kociolek@scramjet.org>
2022-04-07 22:26:48 +0000

    feat(tsconfig) added isolated modules flag and fixed types exports

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,3 +1,4 @@
 export { HostClient } from "./host-client";
-export { InstanceClient, InstanceInputStream, InstanceOutputStream } from "./instance-client";
+export { InstanceClient } from "./instance-client";
+export type { InstanceInputStream, InstanceOutputStream } from "./instance-client";
 export { SequenceClient } from "./sequence-client";
