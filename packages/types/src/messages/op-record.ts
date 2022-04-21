4149166b47e82603d004d5b7d6509e98ca0ed258
Alicja Gruzdz <alicja.gruzdz@gmail.com>
2022-04-21 09:45:53 +0000

    OpRecord attributes description

--- packages/types/src/messages/op-record.ts
+++ packages/types/src/messages/op-record.ts
@@ -1,16 +1,61 @@
 import { OpRecordCode } from "@scramjet/symbols";
 
 export type OpRecord = {
+
+    /**
+    * The type of recorded operation is identified by the code value from the OpRecord enumeration.
+    */
     opCode: OpRecordCode;
+
+    /**
+    * The generated unique request ID for operations coming from the API.
+    */
+
     requestId: string;
+
+    /**
+    * The requestor can be either a user who performed the operation or the system.
+    */
     requestorId: string;
+
+    /**
+    * The address of the original requestor for operations coming from the API .
+    */
     requestorIP?: string;
+
+    /**
+    * The timestamp of when the operation was registered.
+    */
     receivedAt: number;
+
+    /**
+    * The timestamp of when the operation was finished (if applicable).
+    */
     endedAt?: number;
+
+    /**
+    * An instance of the object which the operation concerns, e.g. Host.
+    */
     objectId: string;
+
+    /**
+    * A type of the object which the operation concerns, e.g. Host ID.
+    */
     objectType: string;
+
+    /**
+    * The data written to the request stream. 
+    */
     tx?: number;
+
+    /**
+    * The data received in the request stream
+    */
     rx?: number;
+
+    /**
+    * The full API URL of the operations coming from the API.
+    */
     requestedURL?: string;
 
 }
