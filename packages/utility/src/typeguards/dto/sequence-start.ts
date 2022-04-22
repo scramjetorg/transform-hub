cb68412b9d20e236f769edf28046a9a1e2955c12
Budleigh Salterton <cz@scramjet.org>
2022-04-22 10:54:17 +0200

    Update packages/utility/src/typeguards/dto/sequence-start.ts
    Co-authored-by: Alicja Gruzdz <alicja.gruzdz@gmail.com>

--- packages/utility/src/typeguards/dto/sequence-start.ts
+++ packages/utility/src/typeguards/dto/sequence-start.ts
@@ -8,7 +8,7 @@ export function isStartSequenceDTO(arg: any): arg is StartSequenceDTO {
     if (!["object", "undefined"].includes(typeof appConfig)) throw new Error(`DTO appConfig is ${typeof appConfig}, not an object`);
     if (typeof args !== "undefined") {
         if (!Array.isArray(args)) throw new Error("DTO args are not an array");
-        if ((args as string[]).some(x => typeof x !== "string")) throw new Error("DTO args are all stings");
+        if ((args as string[]).some(x => typeof x !== "string")) throw new Error("DTO args are all strings");
     }
     if (Object.keys(rest).length > 0) throw new Error(`DTO has unknown ${Object.keys(rest)} keys`);
 
