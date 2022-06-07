a155a168382d631ccb0c4ebbd8ab03bb0451f86a
Budleigh Salterton <cz@scramjet.org>
2022-06-07 16:35:04 +0300

    Test fix for windows SI operation (#524)
    Attempts to fix si operation on Windows

--- packages/cli/src/utils/sessionId.ts
+++ packages/cli/src/utils/sessionId.ts
@@ -1,3 +1,8 @@
 import { readFileSync } from "fs";
+import * as os from "os";
 
-export const sessionId = () => readFileSync("/proc/self/stat", { encoding: "utf-8" }).split(" ")[5];
+export const sessionId = () => {
+    if (os.platform() !== "linux") return "";
+
+    return readFileSync("/proc/self/stat", { encoding: "utf-8" }).split(" ")[5];
+};
