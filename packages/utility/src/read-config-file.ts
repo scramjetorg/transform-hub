e0121c4a42bf1eba1ad412183c085cfd2d4c1c60
Budleigh Salterton <cz@scramjet.org>
2022-06-20 21:23:35 +0000

    Fix eslint

--- packages/utility/src/read-config-file.ts
+++ packages/utility/src/read-config-file.ts
@@ -1,13 +1,13 @@
 import { readFile } from "fs/promises";
 import { parseDocument } from "yaml";
 
-export async function readJsonFile(filename: string) {
+async function readJsonFile(filename: string) {
     const configString = await readFile(filename, "utf-8");
 
     return JSON.parse(configString);
 }
 
-export async function readYmlFile(filename: string) {
+async function readYmlFile(filename: string) {
     const configString = await readFile(filename, "utf-8");
 
     return parseDocument(configString);
@@ -25,4 +25,3 @@ export async function readConfigFile(filename: string): Promise<any> {
         return readYmlFile(filename);
     }
 }
-
