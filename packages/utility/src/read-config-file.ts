46ffc095937fb860fee9dd897a2ee78996c26992
Budleigh Salterton <cz@scramjet.org>
2022-06-20 20:22:33 +0000

    Add yaml loader

--- packages/utility/src/read-config-file.ts
+++ packages/utility/src/read-config-file.ts
@@ -1,7 +1,27 @@
 import { readFile } from "fs/promises";
+import { parseDocument } from "yaml";
 
-export async function readConfigFile(filename: string) {
+export async function readConfigFile(filename: string): Promise<any> {
+    if (filename.endsWith(".json")) return readJsonFile(filename);
+    if (filename.endsWith(".yaml") || filename.endsWith(".yml"))
+        return readYmlFile(filename);
+
+    try {
+        // await here on purpose, so try/catch works.
+        return await readJsonFile(filename);
+    } catch {
+        return readYmlFile(filename);
+    }
+}
+
+export async function readJsonFile(filename: string) {
     const configString = await readFile(filename, "utf-8");
 
     return JSON.parse(configString);
+}
+
+export async function readYmlFile(filename: string) {
+    const configString = await readFile(filename, "utf-8");
+
+    return parseDocument(configString);
 }
