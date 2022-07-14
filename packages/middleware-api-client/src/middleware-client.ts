c6a00efc22a1c4d14209f23a76ee2fa698463026
Tomasz Kociołek <tomasz.kociolek@scramjet.org>
2022-07-14 17:53:41 +0000

    docs(middleware-api-client) added jsdoc descriptions to middleware-api-client

--- packages/middleware-api-client/src/middleware-client.ts
+++ packages/middleware-api-client/src/middleware-client.ts
@@ -3,6 +3,10 @@ import { ClientProvider, ClientUtils } from "@scramjet/client-utils";
 import { ManagerClient } from "@scramjet/manager-api-client";
 import { MWRestAPI, MMRestAPI } from "@scramjet/types";
 
+/**
+ * Middleware client.
+ * Client for the Middleware interaction.
+ */
 export class MiddlewareClient implements ClientProvider {
     apiBase: string;
     client: ClientUtils;
@@ -13,14 +17,31 @@ export class MiddlewareClient implements ClientProvider {
         this.client = utils;
     }
 
-    getManagerClient(id: string, mutliManagerApiBase = "/api/v1") {
+    /**
+     * Creates new ManagerClient with proper base api url setup
+     *
+     * @param {string} id Space ID used in apiBase url
+     * @param {string} mutliManagerApiBase api/version prefix by default
+     * @returns {ManagerClient} ManagerClient for space management
+     */
+    getManagerClient(id: string, mutliManagerApiBase = "/api/v1"): ManagerClient {
         return new ManagerClient(`${this.apiBase}/space/${id}${mutliManagerApiBase}`);
     }
 
-    async getManagers() {
+    /**
+     * Requests API for the list of managers avaiable to the user
+     *
+     * @returns {Promise<MMRestAPI.GetManagersResponse>} List of manager ids
+     */
+    async getManagers(): Promise<MMRestAPI.GetManagersResponse> {
         return this.client.get<MMRestAPI.GetManagersResponse>("spaces");
     }
 
+    /**
+     * Requests API for version of various API components
+     *
+     * @returns {Promise<MMRestAPI.GetManagersResponse>} List of manager ids
+     */
     async getVersion(): Promise<MWRestAPI.GetVersionResponse> {
         return this.client.get("version");
     }
