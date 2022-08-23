69c63e90ca45c86e118b842511d97c264e78cee9
patuwwy <patuwwy@gmail.com>
2022-08-23 16:58:39 +0000

    telemetry. postintall.js, allow to provide dist package.json override

--- packages/sth/.eslintrc.js
+++ packages/sth/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    ignorePatterns: [".eslintrc.js"],
+    ignorePatterns: [".eslintrc.js", "postinstall.js"],
     parserOptions:{
         project: "./tsconfig.json",
-        tsconfigRootDir: __dirname
+        tsconfigRootDir: __dirname,
     }
 };
--- packages/sth/src/postinstall.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ts-node
-
-import { defaultConfig } from "@scramjet/sth-config";
-
-if (defaultConfig.telemetry.status) {
-    // eslint-disable-next-line no-console
-    console.log(`Scramjet Transform Hub: Telemetry is ${defaultConfig.telemetry.status}. If you do not want to send anonymous usage data edit the configuration file or use "--no-telemetry" flag when starting STH`);
-}
