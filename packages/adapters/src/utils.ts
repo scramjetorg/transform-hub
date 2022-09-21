c6a0ded5f1b1192b2dc6be776b1da2463d9cd034
Budleigh Salterton <cz@scramjet.org>
2022-09-21 11:35:47 +0200

    Add version to telemetry (#698)
    * Fix language detection.
    
    * Refactor telemetry pushing and add version
    
    * Add hour chime, fix error in detection

--- packages/adapters/src/utils.ts
+++ packages/adapters/src/utils.ts
@@ -6,5 +6,11 @@
  * @param {any} packageJson package.json contents
  * @returns {string} Detected language or "unknown"
  */
-export const detectLanguage = (packageJson: {[key: string]: any}) =>
-    (packageJson.main?.match(/(?:\.)([^.\\/:*?"<>|\r\n]+$)/) || { 1: undefined })[1] || packageJson.engines?.node ? "js" : "unknown";
+export const detectLanguage = (packageJson: {[key: string]: any}) => {
+    if (packageJson.engines) {
+        if ("python3" in packageJson.engines) return "py";
+        if ("node" in packageJson.engines) return "js";
+    }
+
+    return (packageJson.main?.match(/(?:\.)([^.\\/:*?"<>|\r\n]+$)/) || { 1: undefined })[1] || "unknown";
+};
