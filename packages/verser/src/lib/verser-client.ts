308e87081f275735241d234ed60fc2a583f587da
patuwwy <patuwwy@gmail.com>
2022-11-28 15:15:33 +0000

    Test. Enable Nagle

--- packages/verser/src/lib/verser-client.ts
+++ packages/verser/src/lib/verser-client.ts
@@ -103,9 +103,6 @@ export class VerserClient extends TypedEmitter<Events> {
                 ca: typeof this.opts.https === "object" ? this.opts.https.ca : undefined,
             });
 
-            connectRequest.socket?.setTimeout(0);
-            connectRequest.socket?.setNoDelay(true);
-
             connectRequest.on("error", (err) => {
                 this.logger.error("Connect error", err);
                 reject(err);
