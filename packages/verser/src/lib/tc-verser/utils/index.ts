9464e77788acde5ff2e5ec5b40f75eb308df3fd0
patuwwy <patuwwy@gmail.com>
2023-01-10 10:08:50 +0000

    Start for bpmux replacement

---
+++ packages/verser/src/lib/tc-verser/utils/index.ts
@@ -0,0 +1,10 @@
+export function toHex(chunk: Buffer) {
+    return chunk.toString('hex').match(/../g)?.join(' ');
+}
+
+export const HEADER_LENGTH = 32;
+
+export enum FrameTarget {
+    API,
+    INPUT = 1001
+}
