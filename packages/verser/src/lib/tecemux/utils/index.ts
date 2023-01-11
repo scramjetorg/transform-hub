513194eacd3fabdf6ea80907eaafa70b9a54ed03
patuwwy <patuwwy@gmail.com>
2023-01-11 11:10:31 +0000

    DecodedFrame type

--- packages/verser/src/lib/tecemux/utils/index.ts
+++ packages/verser/src/lib/tecemux/utils/index.ts
@@ -8,3 +8,16 @@ export enum FrameTarget {
     API,
     INPUT = 1001
 }
+
+export type DecodedFrame = {
+    sourceAddress: [number, number, number, number];
+    destinationAddress: [number, number, number, number];
+    destinationPort: number;
+    chunk: {
+        type: string;
+        data: any;
+    };
+    dataLength: number;
+    chunkLength: number;
+    stringified: string;
+};
