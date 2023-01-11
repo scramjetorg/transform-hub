f622e035f9d8caa95359dfaf96506c89f71be0ac
patuwwy <patuwwy@gmail.com>
2023-01-11 11:10:31 +0000

    DecodedFrame type

--- packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
+++ packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
@@ -9,8 +9,9 @@ export class FrameDecoder extends Transform {
 
     _streams = new Map<number, Duplex>();
 
-    constructor(opts?: TransformOptions, params: { name: string } = { name: "FrameDecoder" }) {
-        super(opts);
+    constructor(opts: TransformOptions = {}, params: { name: string } = { name: "FrameDecoder" }) {
+        super(Object.assign({}, { readableObjectMode: true }, opts));
+
         this.buff = Buffer.alloc(64 * 1024, 0, undefined); //@TODO: optimize
         this.logger = new ObjLogger(params.name);
 
@@ -34,13 +35,20 @@ export class FrameDecoder extends Transform {
         this.size += chunk.length;
 
         if (this.size >= 10 && this.buff.readInt32LE(10) === this.size) {
-            this.push(JSON.stringify({
-                sourceAddress: `${this.buff.readInt8(0).toString()}.${this.buff.readInt8(1).toString()}.${this.buff.readInt8(2).toString()}.${this.buff.readInt8(3).toString()}`,
-                destinationAddress: `${this.buff.readInt8(4).toString()}.${this.buff.readInt8(5).toString()}.${this.buff.readInt8(6).toString()}.${this.buff.readInt8(7).toString()}`,
-                chunk: this.buff.subarray(32, this.buff.readInt32LE(10) - HEADER_LENGTH + 32),
-                dataLength: this.buff.readInt32LE(10) - HEADER_LENGTH,
-                stringified: this.buff.subarray(32, this.buff.readInt32LE(10) - HEADER_LENGTH + 32).toString()
-            }, null, 0) + "\n");
+            const frameSize = this.buff.readInt32LE(10);
+
+            const payload = {
+                sourceAddress: [this.buff.readInt8(0), this.buff.readInt8(1), this.buff.readInt8(2), this.buff.readInt8(3)],
+                destinationAddress: [this.buff.readInt8(4), this.buff.readInt8(5), this.buff.readInt8(6), this.buff.readInt8(7)],
+                destinationPort: this.buff.readInt8(9),
+                chunk: this.buff.subarray(32, this.buff.readInt32LE(10)),
+                dataLength: frameSize - HEADER_LENGTH,
+                chunkLength: frameSize,
+                stringified: this.buff.subarray(32, frameSize).toString()
+            };
+
+            this.push(JSON.stringify(payload) + "\n");
+            //this.push(payload);
 
             this.size = 0;
             this.buff.fill(0);
