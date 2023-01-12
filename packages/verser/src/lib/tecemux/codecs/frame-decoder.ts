7ccf4e6f6b119724d234edffae01d9b9666500d7
patuwwy <patuwwy@gmail.com>
2023-01-12 20:49:34 +0000

    Flags parsers

--- packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
+++ packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
@@ -1,6 +1,7 @@
 import { ObjLogger } from "@scramjet/obj-logger";
 import { Duplex, Transform, TransformCallback, TransformOptions } from "stream";
 import { HEADER_LENGTH, toHex } from "../utils";
+import { frameFlags } from ".";
 
 export class FrameDecoder extends Transform {
     buff: Buffer;
@@ -9,8 +10,9 @@ export class FrameDecoder extends Transform {
 
     _streams = new Map<number, Duplex>();
 
+
     constructor(opts: TransformOptions = {}, params: { name: string } = { name: "FrameDecoder" }) {
-        super(Object.assign({}, { readableObjectMode: true }, opts));
+        super(Object.assign({}, { readableObjectMode: true, emitClose: false }, opts));
 
         this.buff = Buffer.alloc(64 * 1024, 0, undefined); //@TODO: optimize
         this.logger = new ObjLogger(params.name);
@@ -18,6 +20,11 @@ export class FrameDecoder extends Transform {
         this.on("pipe", () => {
             this.logger.debug("onPipe");
         });
+    }
+
+    parseFlags(byte: number) {
+        return frameFlags.filter((_flag, index) => byte >>> index & 1)
+            .reduce((p, c) => ({ ...p, [c]: true }), {});
     }
 
     _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback): void {
@@ -40,15 +47,16 @@ export class FrameDecoder extends Transform {
             const payload = {
                 sourceAddress: [this.buff.readInt8(0), this.buff.readInt8(1), this.buff.readInt8(2), this.buff.readInt8(3)],
                 destinationAddress: [this.buff.readInt8(4), this.buff.readInt8(5), this.buff.readInt8(6), this.buff.readInt8(7)],
-                destinationPort: this.buff.readInt8(9),
                 chunk: this.buff.subarray(32, this.buff.readInt32LE(10)),
+                flags: this.parseFlags(this.buff.readInt8(25)),
+                sourcePort: this.buff.readInt16LE(12),
+                destinationPort: this.buff.readInt16LE(14),
                 dataLength: frameSize - HEADER_LENGTH,
                 chunkLength: frameSize,
                 stringified: this.buff.subarray(32, frameSize).toString()
             };
 
             this.push(JSON.stringify(payload) + "\n");
-            //this.push(payload);
 
             this.size = 0;
             this.buff.fill(0);
--- packages/verser/src/lib/tecemux/codecs/index.ts
+++ packages/verser/src/lib/tecemux/codecs/index.ts
@@ -1,50 +1,21 @@
-import { Socket } from "net";
 import { Duplex } from "stream";
-import { FrameEncoder } from "./frame-encoder";
-import { FrameTarget } from "../utils";
-import { TypedEmitter } from "@scramjet/utility";
-import { FrameDecoder } from "./frame-decoder";
 
 export * from "./frame-decoder";
 export * from "./frame-encoder";
 
-type TeceMuxEvents = {
+export type TeceMuxEvents = {
     channel(socket: Duplex): void;
+    error(error: any): void;
 }
 
-export class TeceMux extends TypedEmitter<TeceMuxEvents>{
-    socket: Duplex;
-    socketCount = 0;
-    decoder = new FrameDecoder();
-
-    channels: Duplex[] = [];
-
-    constructor(socket: Socket) {
-        super();
-        this.socket = socket;
-
-        this.decoder.on("data", (chunk) => {
-            const frame = JSON.parse(chunk);
-
-            const channel = frame.destinationPort;
-
-            if (!this.channels[channel]) {
-                this.channels[channel] = new Duplex({ });
-                this.channels[channel].pipe(new FrameEncoder(channel).pipe(this.socket)); // ? lot of encoders / encoder.writeToChannel ?;
-                this.emit("channel", this.channels[channel]);
-            }
-
-            this.channels[channel].write(frame.chunk.data);
-        });
-
-        socket.pipe(this.decoder);
-    }
-
-    multiplex() {
-        const multiplex = new Socket();
-
-        multiplex.pipe(new FrameEncoder(FrameTarget.API)).pipe(this.socket);
-
-        return multiplex;
-    }
+export const frameFlags = ["FIN", "SYN", "RST", "PSH", "ACK", "URG", "ECE", "CWR"];
+export const binaryFlags = {
+    FIN: 0b00000001,
+    SYN: 0b00000010,
+    RST: 0b00000100,
+    PSH: 0b00001000,
+    ACK: 0b00010000,
+    URG: 0b00100000,
+    ECE: 0b01000000,
+    CWR: 0b10000000
 }
