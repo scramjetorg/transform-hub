f61d03a530fac157110ab6986f21a2bc828e17cf
patuwwy <patuwwy@gmail.com>
2023-01-13 22:54:35 +0000

    Flags utils

--- packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
+++ packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
@@ -1,7 +1,7 @@
 import { ObjLogger } from "@scramjet/obj-logger";
 import { Duplex, Transform, TransformCallback, TransformOptions } from "stream";
 import { HEADER_LENGTH, toHex } from "../utils";
-import { frameFlags } from ".";
+import { frameFlags, parseFlags } from ".";
 
 export class FrameDecoder extends Transform {
     buff: Buffer;
@@ -20,11 +20,6 @@ export class FrameDecoder extends Transform {
         this.on("pipe", () => {
             this.logger.debug("onPipe");
         });
-    }
-
-    parseFlags(byte: number) {
-        return frameFlags.filter((_flag, index) => byte >>> index & 1)
-            .reduce((p, c) => ({ ...p, [c]: true }), {});
     }
 
     _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback): void {
@@ -48,7 +43,7 @@ export class FrameDecoder extends Transform {
                 sourceAddress: [this.buff.readInt8(0), this.buff.readInt8(1), this.buff.readInt8(2), this.buff.readInt8(3)],
                 destinationAddress: [this.buff.readInt8(4), this.buff.readInt8(5), this.buff.readInt8(6), this.buff.readInt8(7)],
                 chunk: this.buff.subarray(32, this.buff.readInt32LE(10)),
-                flags: this.parseFlags(this.buff.readInt8(25)),
+                flags: parseFlags(this.buff.readInt8(25)),
                 sourcePort: this.buff.readInt16LE(12),
                 destinationPort: this.buff.readInt16LE(14),
                 dataLength: frameSize - HEADER_LENGTH,
