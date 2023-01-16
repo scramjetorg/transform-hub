48151d5108462139bd82c6031fd9102ee36c0f22
patuwwy <patuwwy@gmail.com>
2023-01-16 17:08:47 +0000

    Duplex communication

--- packages/verser/src/lib/tecemux/playground-tecemux.ts
+++ packages/verser/src/lib/tecemux/playground-tecemux.ts
@@ -3,17 +3,21 @@
 /* eslint-disable no-console */
 
 import { ObjLogger, prettyPrint } from "@scramjet/obj-logger";
-import { IncomingMessage, createServer, request } from "http";
+import { IncomingMessage, createServer } from "http";
 import { DataStream } from "scramjet";
-import { FrameData } from "./utils";
 
-import { Socket } from "net";
+import { Socket, createConnection } from "net";
 import { TeceMux, TeceMuxChannel } from "./tecemux";
+import { FrameData } from "./utils";
 
 (async () => {
     const logger = new ObjLogger("Sandbox");
 
     logger.pipe(new DataStream().map(prettyPrint({ colors: true }))).pipe(process.stdout);
+
+    /**********************************************/
+    /* SERVER
+    /**********************************************/
 
     const PORT = 6660;
     const server = createServer();
@@ -22,94 +26,88 @@ import { TeceMux, TeceMuxChannel } from "./tecemux";
     server.requestTimeout = 0;
 
     server.on("connect", async (req: IncomingMessage, socket: Socket) => {
-        //socket.setNoDelay(true);
-        //socket.write(`HTTP/1.1 ${200} \r\n\r\n`);
-        socket.write('HTTP/1.1 200 Connection Established\r\n' +
-        'Proxy-agent: Node.js-Proxy\r\n' +
-        '\r\n');
-        logger.debug("on connect");
-        socket.write("dddd\r\n");
-        //socket.pipe(process.stdout)
+        socket.setNoDelay(true);
 
-        const tcmux = new TeceMux(socket, "Server Side");
-        tcmux.on("error", (err) => {
-            logger.error("TCMUX err", err);
-        });
+        logger.info("Incoming request", req.method, req.headers);
+
+        const tcmux = new TeceMux(socket, "Server Side")
+            .on("error", (err) => {
+                logger.error("TCMUX err", err);
+            });
 
         tcmux.logger.pipe(logger);
 
         const channel = tcmux.multiplex();
 
-        let i = 0;
-
         req.on("pause", () => { logger.warn("Request paused"); });
 
-        setInterval(() => {
-            logger.warn("writing to server response")
-            channel.write("som\n");
-        }, 250)
+        logger.warn("writing to server response")
+        channel.write("som\n");
 
         for await (const chunk of channel) {
             const parsed = JSON.parse(chunk) as FrameData;
-            logger.debug(`Server on request data [${i++}]`, parsed.chunk, parsed.dataLength);
+            logger.debug(`Server on request data [C: ${parsed.sequenceNumber}, SN: ${parsed.sequenceNumber}]`, parsed.chunk, parsed.dataLength);
 
-            channel.write(new Uint8Array([1,2,3,4]));
+            //channel.write(new Uint8Array([1,2,3,4]));
             //channel.write(Buffer.from(new Uint8Array([255, 255, 255, 255])));
         };
-    });
-
-    server.listen(PORT);
-
-    const req = request({
-        hostname: "0.0.0.0",
-        method: "connect",
-        port: PORT,
-        headers: { "Content-Type": "application/octet-stream", "Transfer-Encoding": "chunked" }
-    });
-
-    req.on("connect", (response, socket, head) => {
-        const reqLogger = new ObjLogger("Req", { id: "Request"});
-        reqLogger.pipe(logger);
-
-
-        //socket.setNoDelay(true);
-
-        reqLogger.debug("Response. Head:", head.toString());
-
-        let i = 0;
-        let m = 0;
-        response.on("data", (d) => {
-            console.log("req data in", d);
-        }).pause();
-
-
-        const tcmux = new TeceMux(socket);
-
-        socket.resume();
-        tcmux.logger.updateBaseLog({ id: "Client side"});
-
-        tcmux.logger.pipe(logger);
-
-        tcmux.on("channel", (channel: TeceMuxChannel) => {
-            reqLogger.debug("New channel", channel._id);
-            channel.pipe(process.stdout);
-
-            setInterval(async () => {
-                reqLogger.debug(`Writing [${++m}]..`);
-
-                channel.write(Buffer.from(new Uint8Array([0x61, 0x62, 0x63, 0x64])));
-            }, 500);
+        /*
+        channel.on("data", (d) => {
+            logger.error("Server on request data", d);
         });
-        /*setInterval(async () => {
-            reqLogger.debug(`Writing [${++m}]..`);
-            tcmux.multiplex().write(Buffer.from(new Uint8Array([0x61, 0x62, 0x63, 0x64])));
-        }, 250);*/
-        req.on("error", (err) => {
-            console.error(err);
-        });
+        */
+
+        //channel.pipe(process.stdout);
     });
 
-    req.flushHeaders();
+    server.listen(PORT, "0.0.0.0");
+
+    /**********************************************/
+    /* CLIENT
+    /**********************************************/
+
+    const socket = createConnection({ port: PORT, allowHalfOpen: true, host: "0.0.0.0" }, () => {});
+
+    socket.setNoDelay(true);
+
+    const reqLogger = new ObjLogger("Req", { id: "Request"});
+    reqLogger.pipe(logger);
+
+
+    socket.write("CONNECT HTTP/1.1\r\n\r\n\r\n");
+
+    socket.on("error", (error) => {
+        reqLogger.error("ERROR", error);
+    });
+
+    reqLogger.info('connected to server!');
+
+    const tcmux = new TeceMux(socket);
+
+    tcmux.logger.updateBaseLog({ id: "Client side"});
+
+    tcmux.logger.pipe(logger);
+
+    tcmux.on("channel", async (channel: TeceMuxChannel) => {
+        reqLogger.debug("New channel", channel._id);
+
+        //channel.write(Buffer.from(new Uint8Array([0x61, 0x62, 0x63, 0x64])));
+        channel.pipe(process.stdout);
+
+        for await (const chunk of channel) {
+            reqLogger.info("request on response data", chunk);
+        }
+    });
+    /*setInterval(async () => {
+        reqLogger.debug(`Writing [${++m}]..`);
+        tcmux.multiplex().write(Buffer.from(new Uint8Array([0x61, 0x62, 0x63, 0x64])));
+    }, 250);*/
+    socket.on("error", (err) => {
+        console.error(err);
+    });
+
+
+    //socket.flushHeaders();
 
     await new Promise((_res, _rej) => {});
 })();
