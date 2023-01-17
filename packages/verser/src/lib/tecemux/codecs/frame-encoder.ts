0fa423c13af85367e1fb352f9a54a42160e6cda4
patuwwy <patuwwy@gmail.com>
2023-01-17 21:37:41 +0000

    Debug AbortError

--- packages/verser/src/lib/tecemux/codecs/frame-encoder.ts
+++ packages/verser/src/lib/tecemux/codecs/frame-encoder.ts
@@ -8,7 +8,7 @@ export class FrameEncoder extends Transform {
     logger = new ObjLogger("FrameEncoder",);
     out = new PassThrough({ readableObjectMode: true })
         .on("data", (data) => {
-            this.logger.trace("output to socket:", toHex(data), data.length, this.readableFlowing);
+            this.logger.trace("output to socket: " + (data.length == HEADER_LENGTH ? "HEADER ONLY" : ""), toHex(data), data.length, this.readableFlowing);
         })
         .on("pause", () => {
             this.logger.trace("output to socket paused");
@@ -19,9 +19,12 @@ export class FrameEncoder extends Transform {
         .on("resume", () => {
             this.logger.trace("output to socket resumed");
         })
+        .on("error", (error) => {
+            this.logger.error("output to socket paused", error);
+        })
 
     constructor(private frameTarget: FrameTarget, opts: TransformOptions = {}, params: { name: string } = { name: "FrameEncoder" }) {
-        opts.emitClose = false;
+        //opts.emitClose = false;
 
         super(opts);
 
@@ -102,53 +105,11 @@ export class FrameEncoder extends Transform {
         return buffer;
     }
 
-    createChunkFrame(chunk: any) {
-        const checksum = this.getChecksum();
-        const buffer = Buffer.concat([
-            // 0: source address 0 - 3
-            new Uint8Array([10, 0, 0, 1]),
-            // 32: destination address 4 - 7
-            new Uint8Array([10, 0, 0, 2]),
-
-            // 64: zeroes (8bit), protocol (8bit), 8 - 9
-            new Uint8Array([0, 1]),
-            // tcp length (16bit) 10 - 11
-            new Uint8Array(new Uint16Array([chunk.length + HEADER_LENGTH]).buffer),
-
-            // 96: Source port,	destination port 12 - 15
-            new Uint8Array(new Uint16Array([0, this.frameTarget]).buffer),
-
-            // 128: sequenceNumber(32 bit, acnowledge number) 16 - 19
-            new Uint8Array(new Uint32Array([this.sequenceNumber++]).buffer),
-
-            // 160: Acknowledgement number 20-23
-            new Uint8Array([0, 0, 0, 0]),
-
-            // 192: data offset (4bit), reserved (4bit), 24
-            new Uint8Array([0b00000000]),
-            // 224: flags (8bit), 25
-            this.setFlags(["PSH"]),
-            // window(16bit) 26 - 27
-            new Uint8Array(new Uint16Array([0]).buffer),
-
-            // checksum(16bit) 28 - 29
-            new Uint8Array(new Uint16Array([checksum]).buffer),
-            // pointer (16bit) 30 - 31
-            new Uint8Array(new Uint16Array([checksum]).buffer),
-
-            // 256: data 32 -
-            new Uint8Array(chunk)
-        ]);
-
-        return buffer;
-    }
-
     _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void {
         //const buffer = this.createChunkFrame(chunk);
         this.logger.debug("TRANSFORM IN", toHex(chunk), chunk.length);
         const buffer = this.createFrame(chunk, { destinationPort: this.frameTarget, flagsArray: ["PSH"] });
 
-        //this.push(buffer, undefined);
         this.logger.debug("TRANSFORM OUT", getHexString(buffer), "Size: ", buffer.length);
         this.push(buffer, undefined);
         callback();
