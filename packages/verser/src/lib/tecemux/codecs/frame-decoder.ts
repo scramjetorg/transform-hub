9becfad6dff5242bb8733849b530879d07221e1c
patuwwy <patuwwy@gmail.com>
2023-01-18 22:05:00 +0000

    Issue. Decoder receives more than 1 frame

--- packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
+++ packages/verser/src/lib/tecemux/codecs/frame-decoder.ts
@@ -11,7 +11,7 @@ export class FrameDecoder extends Transform {
     _streams = new Map<number, Duplex>();
 
     constructor(opts: TransformOptions = {}, params: { name: string } = { name: "FrameDecoder" }) {
-        super(Object.assign({}, opts, { writableObjectMode: false, readableObjectMode: false }));
+        super(Object.assign({}, opts, { writableObjectMode: true, readableObjectMode: true, readableHighWaterMark: 2 }));
 
         this.buff = Buffer.alloc(64 * 1024, 0, undefined); //@TODO: optimize
         this.logger = new ObjLogger(params.name);
@@ -36,33 +36,33 @@ export class FrameDecoder extends Transform {
 
             if (Buffer.isBuffer(chunk)) {
                 chunk.copy(this.buff, this.size, 0, chunk.length);
+            } else {
+                this.emit("error", "Chunk is not a buffer");
+                callback();
             }
 
             this.size += chunk.length;
 
-            if (this.size >= 10 ) {//&& this.buff.readInt32LE(10) === this.size) {
-                const frameSize = this.buff.readInt32LE(10);
+            const frameSize = this.buff.readInt32LE(10);
 
-                const payload = {
-                    sourceAddress: [this.buff.readInt8(0), this.buff.readInt8(1), this.buff.readInt8(2), this.buff.readInt8(3)],
-                    destinationAddress: [this.buff.readInt8(4), this.buff.readInt8(5), this.buff.readInt8(6), this.buff.readInt8(7)],
-                    chunk: this.buff.subarray(32, this.buff.readInt32LE(10)),
-                    flags: parseFlags(this.buff.readInt8(25)),
-                    sourcePort: this.buff.readInt16LE(12),
-                    destinationPort: this.buff.readInt16LE(14),
-                    dataLength: frameSize - HEADER_LENGTH,
-                    chunkLength: frameSize,
-                    sequenceNumber: this.buff.readInt32LE(16),
-                    stringified: this.buff.subarray(32, frameSize).toString()
-                } as Partial<FrameData>;
+            const payload = {
+                sourceAddress: [this.buff.readInt8(0), this.buff.readInt8(1), this.buff.readInt8(2), this.buff.readInt8(3)],
+                destinationAddress: [this.buff.readInt8(4), this.buff.readInt8(5), this.buff.readInt8(6), this.buff.readInt8(7)],
+                chunk: this.buff.subarray(32, this.buff.readInt32LE(10)),
+                flags: parseFlags(this.buff.readInt8(25)),
+                sourcePort: this.buff.readInt16LE(12),
+                destinationPort: this.buff.readInt16LE(14),
+                dataLength: frameSize - HEADER_LENGTH,
+                chunkLength: frameSize,
+                sequenceNumber: this.buff.readInt32LE(16),
+                stringified: this.buff.subarray(32, frameSize).toString()
+            } as Partial<FrameData>;
 
-                this.push(JSON.stringify(payload) + "\n");
+            this.push(JSON.stringify(payload) + "\n");
 
-                this.size = 0;
-                this.buff.fill(0);
-            } else {
-                this.logger.error("too few data", this.size, this.buff.readInt32LE(10));
-            }
+            this.size = 0;
+            this.buff.fill(0);
+
 
             callback();
         } catch(err) {
