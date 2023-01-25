4ac4a721bad132e653a7e95424359ff9ca61797f
patuwwy <patuwwy@gmail.com>
2023-01-25 13:02:13 +0000

    Playground pass file

---
+++ packages/verser/src/lib/tecemux/frames-keeper.ts
@@ -0,0 +1,36 @@
+import { ObjLogger } from "@scramjet/obj-logger";
+import { PassThrough } from "stream";
+
+export class FramesKeeper extends PassThrough {
+    logger = new ObjLogger(this);
+    archive = new Map<number, Buffer>();
+    lastSequenceNumberSent: number = -1;
+
+    _write(chunk: any, encoding: BufferEncoding, cb: ((error: Error | null | undefined) => void) | undefined) {
+        const sequenceNumber = chunk.readInt32LE(16);
+        this.archive.set(sequenceNumber, chunk);
+        this.logger.debug(`sequenceNumber ${sequenceNumber} stored, size: ${chunk.length}`);
+
+        this.push(chunk, encoding);
+        if (cb) cb(undefined);
+
+        this.lastSequenceNumberSent = sequenceNumber;
+
+        // await new Promise<void>((resolve, reject) => {
+        //     while (this.archive.get(sequenceNumber)) {
+        //         this.logger.debug("wait for ack for", sequenceNumber)
+        //     }
+        //     resolve();
+        // });
+
+        return true
+    }
+
+    _read(size: number) {
+    }
+
+    onACK(sequenceNumber: number) {
+        this.logger.debug("onACK", sequenceNumber);
+        this.archive.delete(sequenceNumber);
+    }
+}
