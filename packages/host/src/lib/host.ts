58af95be2abfb9dff4204dd1ec1ffe942196c82f
patuwwy <patuwwy@gmail.com>
2023-02-28 11:40:35 +0000

    Add hub type, org:space param

--- packages/host/src/lib/host.ts
+++ packages/host/src/lib/host.ts
@@ -331,8 +331,7 @@ export class Host implements IComponent {
         if ((this.config.cpmUrl || this.config.platform?.api) && (this.config.cpmId || this.config.platform?.space)) {
             this.cpmConnector = new CPMConnector(
                 this.config.platform?.api || this.config.cpmUrl,
-                this.config.platform?.space || this.config.cpmId,
-                this.config.platform?.organisation,
+                this.config.platform?.space || (":" + this.config.cpmId),
                 {
                     selfHosted: this.config.selfHosted,
                     description: this.config.description,
@@ -342,7 +341,8 @@ export class Host implements IComponent {
                     cpmSslCaPath: this.config.cpmSslCaPath,
                     maxReconnections: this.config.cpm.maxReconnections,
                     reconnectionDelay: this.config.cpm.reconnectionDelay,
-                    apiKey: this.config.platform?.api ? this.config.platform?.apiKey : undefined
+                    apiKey: this.config.platform?.api ? this.config.platform?.apiKey : undefined,
+                    hostType: this.config.platform?.hostType
                 },
                 this.api.server
             );
