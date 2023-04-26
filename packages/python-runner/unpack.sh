5d2ee4895bdfe6fc2a021d96d634f981571296ee
S4adam <63969229+S4adam@users.noreply.github.com>
2023-04-26 10:37:52 +0200

    Fix "sequence unpack failed" error with kubernetes (#852)
    * change stderr redirection
    ---------
    
    Co-authored-by: S4Adam <adam.mikolajewski@scramjet.org>
    Co-authored-by: Budleigh Salterton <cz@scramjet.org>

--- packages/python-runner/unpack.sh
+++ packages/python-runner/unpack.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-tar zxf - -C /package 2> $PACKAGE_DIR/.fail && touch /package/.ready || touch $PACKAGE_DIR/.fail
+tar zxf - -C /package && touch /package/.ready || touch $PACKAGE_DIR/.fail
