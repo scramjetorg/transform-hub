e5ef68d5637923f6515a9ec8cbb82e9dce50187c
Piotr <piotrek.falba@gmail.com>
2023-05-17 12:47:55 +0000

    New Cli command that allows restarting instance

--- packages/cli/src/lib/commands/instance.ts
+++ packages/cli/src/lib/commands/instance.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { instanceKill } from "../helpers/instance";
+import { instanceKill, instanceRestart } from "../helpers/instance";
 import { CommandDefinition } from "../../types";
 import { attachStdio, getHostClient, getInstance, getReadStreamFromFile } from "../common";
 import { getInstanceId, profileManager, sessionConfig } from "../config";
@@ -205,5 +205,14 @@ export const instance: CommandDefinition = (program) => {
         .description("Pipe the running Instance stdout stream to stdout")
         .action((id: string) => {
             return displayStream(getInstance(getInstanceId(id)).getStream("stdout"));
+        });
+    instanceCmd
+        .command("restart")
+        .argument("<id>", "Instance id or '-' for the last one started or selected")
+        .description("Kills the instance and start the new one from root sequence")
+        .action(async (id: string) => {
+            const instanceRestartResponse = await instanceRestart(id);
+
+            displayObject(instanceRestartResponse, profileManager.getProfileConfig().format);
         });
 };
--- packages/cli/src/lib/helpers/instance.ts
+++ packages/cli/src/lib/helpers/instance.ts
@@ -1,10 +1,13 @@
 import { STHRestAPI } from "@scramjet/types";
-import { getInstance } from "../common";
+import { getHostClient, getInstance } from "../common";
 import { getInstanceId, sessionConfig } from "../config";
+import { SequenceClient } from "@scramjet/api-client";
+import { defer } from "@scramjet/utility";
+//import { ConfigFileDefault } from "@scramjet/utility";
 
 /**
  * Wraper for kill instance method with config safety checks.
- * 
+ *
  * @param id Id of instance or '-' for last used
  * @param removeImmediately If true, Instance lifetime extension delay will be bypassed.
  * @param lastInstanceId Id of last used instance in session
@@ -22,4 +25,33 @@ export const instanceKill = async (
         sessionConfig.setLastInstanceId("");
     }
     return instanseKillResponse;
+};
+export const instanceRestart = async (
+    instanceId: string
+) => {
+    const instanceInfo = await getInstance(instanceId).getInfo();
+    const sequenceId = instanceInfo.sequence;
+    const sequenceClient = SequenceClient.from(sequenceId, getHostClient());
+    const args = instanceInfo.args;
+    const appConfig = instanceInfo.appConfig || {};
+    const killResponse: STHRestAPI.SendKillInstanceResponse = await instanceKill(instanceId, true);
+    let seqStartResponse: STHRestAPI.StartSequenceResponse = { id: "" };
+    let ready = false;
+
+    while (!ready) {
+        try {
+            await getInstance(instanceId).getInfo();
+            await defer(100);
+        } catch (e:any) {
+            if (e.status === "404") {
+                ready = true;
+                break;
+            }
+        }
+    }
+    if (typeof sequenceId === "string") {
+        seqStartResponse = await sequenceClient.start({ instanceId, appConfig, args });
+    }
+
+    return { killResponse, seqStartResponse };
 };
