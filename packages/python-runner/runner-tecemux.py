fbc48f4a8b7d249ecc713b902ed55865f8f94605
Radosław Gierwiało <radoslaw.gierwialo@scramjet.org>
2023-05-17 13:15:32 +0200

    Logger with coroutine support

--- packages/python-runner/runner-tecemux.py
+++ packages/python-runner/runner-tecemux.py
@@ -2,50 +2,76 @@ import asyncio
 import sys
 import codecs
 import socket
-from attrs import define
+from attrs import define, field
+
 from logging_setup import LoggingSetup
 from hardcoded_magic_values import CommunicationChannels as CC
+
+def get_logger():
+    if not hasattr(get_logger, "log_setup"):
+        get_logger.log_setup = LoggingSetup(sys.stdout)
+    return get_logger.log_setup.logger
 
 sequence_path = '/tmp/'
 server_port = 8001
 server_host = 'localhost'
 instance_id = '1234'
 
+
 @define
-class TeceMuxChannelContext:
-    _name: str
-    _queue: asyncio.Queue
-    _reader: asyncio.StreamReader
-    _writer: asyncio.StreamWriter
-
-    _task_reader: asyncio.coroutine = None
-    _task_writer: asyncio.coroutine = None
-
-
-    def get_name(self):
-        return self._name.name
-
 class Tecemux:
-    
-    def __init__(self, reader,writer):
-        
-        self._queue = asyncio.Queue()
+    @define
+    class ChannelContext:
+        _name: str
+        _queue: asyncio.Queue
+        _reader: asyncio.StreamReader
+        _writer: asyncio.StreamWriter
+
+        _task_reader: asyncio.coroutine = None
+        _task_writer: asyncio.coroutine = None
+
+
+        def get_name(self):
+            return self._name.name
+
+    _queue: asyncio.Queue = asyncio.Queue()
+    _reader: asyncio.StreamReader = field(default=None)
+    _writer: asyncio.StreamWriter = field(default=None)
+    _logger = field(default=get_logger())
+    _channels = field(default={})
+
+    async def connect(self, reader, writer):
         self._reader = reader
         self._writer = writer
-        self._channels = None
 
+
+    @staticmethod
+    async def prepare_tcp_connection(server_host, server_port):
+        return await asyncio.open_connection(server_host, server_port)
+    
+    @staticmethod
+    async def prepare_socket_connection():
+        rsock, wsock = socket.socketpair()
+        reader, _ = await asyncio.open_unix_connection(sock=rsock)
+        _, writer = await asyncio.open_unix_connection(sock=wsock)
+        return reader,writer
+    
     async def prepare(self):
-        async def prepare_channel(name):
-            rsock, wsock = socket.socketpair()
-            reader, _ = await asyncio.open_unix_connection(sock=rsock)
-            _, writer = await asyncio.open_unix_connection(sock=wsock)
-            return TeceMuxChannelContext(name, reader,writer,asyncio.Queue())
-
-        self._channels = {channel : await prepare_channel(channel) for channel in CC }
+        self._channels = {channel : Tecemux.ChannelContext(channel, *await Tecemux.prepare_socket_connection(),asyncio.Queue()) for channel in CC }
 
     def get_channel(self, channel):
         return self._channels[channel]
     
+    async def sandbox(self):
+        self._logger.debug("Begin sandbox")
+
+        await self._queue.put(b'test')
+        await asyncio.sleep(0)
+
+        self._writer.write(b'test 2')
+        await self._writer.drain()
+
+        self._logger.debug("End sandbox")
     async def loop(self):
        
         loop = asyncio.get_event_loop()
@@ -58,42 +84,46 @@ class Tecemux:
 
         protocol_reader = loop.create_task(Tecemux.protocol_reader(self._reader, self._queue))
         protocol_writer = loop.create_task(Tecemux.protocol_writer(self._writer, self._queue))
+        sandbox = loop.create_task(self.sandbox())
 
-        await asyncio.gather(*[protocol_reader, protocol_writer])
+        await asyncio.gather(*[protocol_reader, protocol_writer, sandbox])
         print('End main loop')
     
 
     @staticmethod
     async def protocol_reader(reader, queue):
         while True:
-            print(f'PROTOCOL: WRITE!')
-            await asyncio.sleep(0)
+            chunk = await reader.read(100)
+            get_logger().debug(f'Chunk received: {chunk}')
+            await queue.put(chunk)
+            get_logger().debug(f'Chunk redirected: {chunk}')
 
     @staticmethod
     async def protocol_writer(writer, queue):
         while True:
-            print(f'PROTOCOL: READ!')
-            await asyncio.sleep(0)
+            chunk = await queue.get()
+            get_logger().debug(f'PROTOCOL: Chunk waiting: {chunk}')
+            #writer.write(chunk)
+            queue.task_done()
+            get_logger().debug(f'PROTOCOL: Chunk consumed: {chunk}')
 
     @staticmethod
     async def channel_reader(channel_context):
         while True:
-            print(f'{channel_context.get_name()}: READ!')
+            #print(f'{channel_context.get_name()}: READ!')
             await asyncio.sleep(0)
     
     @staticmethod
     async def channel_writer(channel_context):
         while True:
-            print(f'{channel_context.get_name()}: WRITE!')
+            #print(f'{channel_context.get_name()}: WRITE!')
             await asyncio.sleep(0)
     
 class Runner:
-    def __init__(self, instance_id, sequence_path, log_setup) -> None:
+    def __init__(self, instance_id, sequence_path, logger) -> None:
         self.instance_id = instance_id
         self.seq_path = sequence_path
-        self._logging_setup = log_setup
-        
-        self.logger = log_setup.logger
+        self.logger = logger
         
         self.protocol = None
 
@@ -109,8 +139,14 @@ class Runner:
     async def main(self, server_host, server_port):
         self.logger.info('Connecting to host...')
 
-        self.logger.debug(f'Connecting to {server_host}:{server_port}...')
-        self.protocol = Tecemux(*await asyncio.open_connection(server_host, server_port))
+        self.protocol = Tecemux()
+
+        # self.logger.debug(f'Connecting to {server_host}:{server_port}...')
+        # await self.protocol.connect(*await Tecemux.prepare_tcp_connection(server_host, server_port))
+        
+        self.logger.debug(f'Connecting locally via unix sockets')
+        await self.protocol.connect(*await Tecemux.prepare_socket_connection())
+        
         self.logger.debug('Connected.')
 
         await self.protocol.prepare()
@@ -120,5 +156,5 @@ class Runner:
         await self.protocol.loop()
 
 
-runner = Runner(instance_id, sequence_path, LoggingSetup(sys.stdout))
+runner = Runner(instance_id, sequence_path, get_logger())
 asyncio.run(runner.main(server_host, server_port))
