48543a20a4e6f7d63f9dde49825cedafe9f6a50f
leVermito <108662689+leVermito@users.noreply.github.com>
2023-06-05 11:45:42 +0200

    fix/proper-permissions-after-unpack

--- packages/runner/unpack.sh
+++ packages/runner/unpack.sh
@@ -4,4 +4,4 @@ PACKAGE_DIR=${PACKAGE_DIR:-/package}
 
 set -e
 
-tar zxf - -C $PACKAGE_DIR && touch $PACKAGE_DIR/.ready || touch $PACKAGE_DIR/.fail
+tar zxf - -C $PACKAGE_DIR && chown -R 1200:1200 $PACKAGE_DIR && touch $PACKAGE_DIR/.ready || touch $PACKAGE_DIR/.fail
