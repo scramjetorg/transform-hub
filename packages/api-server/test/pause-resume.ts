e1c90eced216ceaf4c7c21c72f43951dd146884e
Budleigh Salterton <cz@scramjet.org>
2023-08-21 22:53:44 +0000

    Fix removal of observe-stream - to be introed in another PR

--- packages/api-server/test/pause-resume.ts
+++ packages/api-server/test/pause-resume.ts
@@ -1,13 +1,10 @@
 import { PassThrough } from "stream";
 import { createServer } from "..";
-import { observe } from "@scramjet/utility/src/observe-stream";
 
 const srv = createServer({
     verbose: true
 });
 const pipe = new PassThrough();
-
-observe(pipe);
 
 srv.downstream("/in", pipe, { text: true });
 srv.upstream("/out", pipe, { text: true });
