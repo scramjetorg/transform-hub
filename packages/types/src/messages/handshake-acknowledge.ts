0ae071543d3f646d0b6ea74d4ff7e5fdee09269b
Budleigh Salterton <cz@scramjet.org>
2024-03-19 11:48:11 +0000

    Allow runner log level change

--- packages/types/src/messages/handshake-acknowledge.ts
+++ packages/types/src/messages/handshake-acknowledge.ts
@@ -1,11 +1,13 @@
 import { RunnerMessageCode } from "@scramjet/symbols";
 import { AppConfig } from "../app-config";
+import { LogLevel } from "../object-logger";
 
 export type HandshakeAcknowledgeMessageData = {
 
     /** Sequence configuration passed to the Sequence when it is started by the Runner. */
     appConfig: AppConfig;
-    args?: any[]
+    args?: any[],
+    logLevel?: LogLevel;
 }
 
 /**
