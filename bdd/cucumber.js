c962c0a422c22015aa48807cf4a72229a0a2236c
Budleigh Salterton <cz@signicode.com>
2025-05-07 23:29:11 +0000

    Don't create reports on basic test runs

--- bdd/cucumber.js
+++ bdd/cucumber.js
@@ -1,11 +1,15 @@
+const reportFileName = new Date().toISOString().replace(new RegExp(/[:\\.]/g), "_") + "_report.html";
+const report = process.env.TEST_REPORT
+    ? [ "--format @cucumber/pretty-formatter", "--format html:reports/" + reportFileName ]
+    : [ "--format null" ];
+
 const common = [
     "--require step-definitions/**/*.ts",
     "--require-module ts-node/register",
     "--publish-quiet",
     "--exit",
     "--tags \"not @ignore\"",
-    "--format @cucumber/pretty-formatter",
-    "--format html:reports/" + new Date().toISOString().replace(new RegExp(/[:\\.]/g), "_") + "_report.html"
+    ...report
 ].join(" ");
 
 module.exports = {
