59e55871850026db6bf39bc3492f86577a099ead
Budleigh Salterton <cz@signicode.com>
2025-06-14 14:58:44 +0000

    Fix docker build and object logger typings.

--- packages/types/src/object-logger.ts
+++ packages/types/src/object-logger.ts
@@ -1,3 +1,5 @@
+import { Readable } from "stream";
+
 export type LogLevel = "ERROR" | "WARN" | "INFO" | "DEBUG" | "FATAL" | "TRACE";
 
 /**
@@ -64,7 +66,7 @@ export interface IObjectLogger {
     warn(entry: LogEntry | string, ...optionalParams: any[]): void;
 
     addObjectLoggerSource(source: IObjectLogger): void;
-    addSerializedLoggerSource(source: NodeJS.ReadableStream): void;
+    addSerializedLoggerSource(source: Readable): void;
 
     pipe(target: NodeJS.WritableStream | IObjectLogger, options?: IObjectLoggerOptions): void;
     unpipe(target?: NodeJS.WritableStream | IObjectLogger): void;
